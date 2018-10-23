23ed1a0b01e802768a6e24b25cbbb4a3fb7489e8
Benjamin Follis <bfollis@uclusion.com>
2018-10-22 21:07:49 -0700

    Moved investibles to market Investibles because we need to account for draft investibles, and the investibles we currently have are all bound to a market

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import initState from './init'
 import { appReducers } from 'uclusion-shell/lib/store/reducers'
 import rootReducer from 'uclusion-shell/lib/store/rootReducer'
-import investiblesReducer from '../containers/Investibles/reducer'
+import investiblesReducer from '../containers/MarketInvestibles/reducer'
 import marketsReducer from '../containers/Markets/reducer'
 import usersReducer from '../containers/Users/reducer'
 

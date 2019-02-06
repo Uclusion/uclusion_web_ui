5f3e00c2c7f372660183304c96baad6c4098feb3
disrael <disrael@uclusion.com>
2019-02-06 11:21:01 -0800

    Fix npe on login

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,20 +1,20 @@
 export const getUclusionLocalStorage = () => {
   const key = Object.keys(localStorage).find(e => e.match(/uclusion:root/))
   const data = JSON.parse(localStorage.getItem(key))
-  return data;
+  return data
 }
 
 export const getUclusionLocalStorageItem = (subkey) => {
   const key = Object.keys(localStorage).find(e => e.match(/uclusion:root/))
   const data = JSON.parse(localStorage.getItem(key))
-  return data[subkey];
+  return data && subkey in data ? data[subkey] : null
 }
 
 export const setUclusionLocalStorageItem = (key, value) => {
   let data = getUclusionLocalStorage()
-  if(!data){
+  if (!data) {
     data = {}
   }
-  data[key] = value;
-  localStorage.setItem('uclusion:root', JSON.stringify(data));
+  data[key] = value
+  localStorage.setItem('uclusion:root', JSON.stringify(data))
 }

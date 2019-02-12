3acc823ab3c4fa738b477f6829b1ba8f51785d32
disrael <disrael@uclusion.com>
2019-02-12 08:52:18 -0800

    Anonymous login based on market allowing

--- src/pages/Login/LoginModal.js
+++ src/pages/Login/LoginModal.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { OidcAuthorizer, SsoAuthorizer, AnonymousAuthorizer } from 'uclusion_authorizer_sdk';
 import {
   Button, Dialog, DialogTitle, List, ListItem,
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { injectIntl } from 'react-intl';
+import { connect } from 'react-redux';
 import appConfig from '../../config/config';
 import { getAuthMarketId, getMarketId } from '../../utils/marketIdPathFunctions';
 import { postAuthTasks } from '../../utils/fetchFunctions';
-import { connect } from 'react-redux';
 import { withBackgroundProcesses } from '../../components/BackgroundProcesses/BackgroundProcessWrapper';
 
 const styles = theme => ({
@@ -17,8 +17,9 @@ const styles = theme => ({
   },
 });
 
-class LoginModal extends Component {
-  getDestinationPage(subPath) {
+function LoginModal(props) {
+  const [allowGuestLogin, setAllowGuestLogin] = useState(false);
+  function getDestinationPage(subPath) {
     const marketId = getMarketId();
     const newPath = `/${marketId}/${subPath}`;
     const currentPage = new URL(window.location.href);
@@ -26,16 +27,15 @@ class LoginModal extends Component {
     currentPage.search = '';
     return currentPage.toString();
   }
-
-  getLoginParams() {
+  function getLoginParams() {
     const marketId = getAuthMarketId();
     const parsed = new URL(window.location.href);
     let page = 'investibles';
     if (parsed.search.includes('destinationPage')) {
       page = parsed.search.split('=')[1];
     }
-    const destinationPage = this.getDestinationPage(page);
-    const redirectUrl = this.getDestinationPage('post_auth');
+    const destinationPage = getDestinationPage(page);
+    const redirectUrl = getDestinationPage('post_auth');
     const pageUrl = window.location.href;
     const uclusionUrl = appConfig.api_configuration.baseURL;
     return {
@@ -46,8 +46,16 @@ class LoginModal extends Component {
       uclusionUrl,
     };
   }
-
-  doLoginRedirect(authorizer, loginParams) {
+  useEffect(() => {
+    const loginParams = getLoginParams();
+    const authorizer = new AnonymousAuthorizer(loginParams);
+    authorizer.marketLoginInfo().then((response) => {
+      const allowAnonymous = response.allow_anonymous;
+      setAllowGuestLogin(allowAnonymous);
+    });
+    return () => {};
+  });
+  function doLoginRedirect(authorizer, loginParams) {
     const { pageUrl, destinationPage, redirectUrl } = loginParams;
     const redirectPromise = authorizer.authorize(pageUrl, destinationPage, redirectUrl);
     redirectPromise.then((location) => {
@@ -56,21 +64,21 @@ class LoginModal extends Component {
     });
   }
 
-  loginOidc = () => {
-    const loginParams = this.getLoginParams();
+  function loginOidc() {
+    const loginParams = getLoginParams();
     const authorizer = new OidcAuthorizer(loginParams);
-    this.doLoginRedirect(authorizer, loginParams);
-  };
+    doLoginRedirect(authorizer, loginParams);
+  }
 
-  loginSso = () => {
-    const loginParams = this.getLoginParams();
+  function loginSso() {
+    const loginParams = getLoginParams();
     const authorizer = new SsoAuthorizer(loginParams);
-    this.doLoginRedirect(authorizer, loginParams);
-  };
+    doLoginRedirect(authorizer, loginParams);
+  }
 
-  loginAnonymous = () => {
-    const { dispatch, webSocket } = this.props;
-    const loginParams = this.getLoginParams();
+  function loginAnonymous() {
+    const { dispatch, webSocket } = props;
+    const loginParams = getLoginParams();
     const authorizer = new AnonymousAuthorizer(loginParams);
     authorizer.doPostAuthorize().then((resolve) => {
       const {
@@ -78,51 +86,51 @@ class LoginModal extends Component {
       } = resolve;
       postAuthTasks(uclusion_token, authorizer.getType(), dispatch, market_id, user, webSocket);
     });
-  };
+  }
 
-  render() {
-    const { intl, classes, ...other } = this.props;
-
-    return (
-      <Dialog onClose={() => null} aria-labelledby="simple-dialog-title" {...other}>
-        <DialogTitle id="simple-dialog-title">Log In</DialogTitle>
-        <div>
-          <List>
+  const { intl, classes, ...other } = props;
+  return (
+    <Dialog onClose={() => null} aria-labelledby="simple-dialog-title" {...other}>
+      <DialogTitle id="simple-dialog-title">Log In</DialogTitle>
+      <div>
+        <List>
+          <ListItem>
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+              onClick={loginOidc}
+            >
+              {intl.formatMessage({ id: 'login_admin' })}
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+              onClick={loginSso}
+            >
+              {intl.formatMessage({ id: 'login_user' })}
+            </Button>
+          </ListItem>
+          {allowGuestLogin
+          && (
             <ListItem>
               <Button
                 className={classes.button}
                 variant="contained"
                 color="primary"
-                onClick={this.loginOidc}
-              >
-                {intl.formatMessage({ id: 'login_admin' })}
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Button
-                className={classes.button}
-                variant="contained"
-                color="primary"
-                onClick={this.loginSso}
-              >
-                {intl.formatMessage({ id: 'login_user' })}
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Button
-                className={classes.button}
-                variant="contained"
-                color="primary"
-                onClick={this.loginAnonymous}
+                onClick={loginAnonymous}
               >
                 {intl.formatMessage({ id: 'login_guest' })}
               </Button>
             </ListItem>
-          </List>
-        </div>
-      </Dialog>
-    );
-  }
+          )}
+        </List>
+      </div>
+    </Dialog>
+  );
 }
 
 function mapDispatchToProps(dispatch) {
@@ -133,4 +141,5 @@ function mapStateToProps(state) {
   return { ...state };
 }
 
-export default withBackgroundProcesses(withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(injectIntl(LoginModal))));
+export default withBackgroundProcesses(withStyles(styles)(connect(mapStateToProps,
+  mapDispatchToProps)(injectIntl(LoginModal))));

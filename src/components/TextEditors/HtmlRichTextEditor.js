178f53f84f343552ad35b9e567382245b10e2cf3
Ben Follis <bfollis@uclusion.com>
2019-02-13 20:57:09 -0800

    Fixed html rich text editor not rerendering if the value prop changed

--- src/components/TextEditors/HtmlRichTextEditor.js
+++ src/components/TextEditors/HtmlRichTextEditor.js
@@ -1,6 +1,6 @@
 /**
  Html RTF editor based on slate, that's we can ue wherever we need full text editing
-It accepts an onChange property, which will return the string HTML value of the current state of the editor,
+ It accepts an onChange property, which will return the string HTML value of the current state of the editor,
  the initialText property which will be the text string renedered if no existing state is available,
  and an ObjectId which will be used to search for state that hasn't been flushed (eg. we reloaded) without saving
  It's onchange handler emulates the material ui input type onChange emission
@@ -26,6 +26,16 @@ class HtmlRichTextEditor extends React.Component {
     };
   }
 
+  /** This is a dirty hack to hook into the update cycle in case the upstream value props changes
+   * This code should really look into switching to hooks
+   */
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    if (nextProps.value !== this.props.value) {
+      this.setState({ value: this.html.deserialize(nextProps.value) });
+    }
+    return true;
+  }
+
   onChange = ({ value }) => {
     const { readOnly, onChange } = this.props;
     if (!readOnly) {
@@ -43,7 +53,7 @@ class HtmlRichTextEditor extends React.Component {
 
       this.setState({ value });
     }
-  }
+  };
 
   // Render the editor.
   render() {

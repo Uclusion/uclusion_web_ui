8c02b05cb6d09b327a2f23036b9377d3e46563d6
Benjamin Follis <bfollis@uclusion.com>
2019-02-14 14:37:38 -0800

    Made reducer reformat the investibles in it's storage

--- src/store/MarketInvestibles/reducer.js
+++ src/store/MarketInvestibles/reducer.js
@@ -7,70 +7,80 @@ import {
   INVESTIBLE_CREATED, MARKET_INVESTIBLE_CREATED, MARKET_INVESTIBLE_DELETED,
 } from './actions';
 
-const reFormatInvestible = (investible) => {
+function reFormatInvestible(investible){
   investible.created_at = new Date(investible.created_at);
   investible.updated_at = new Date(investible.updated_at);
   investible.last_investment_at = new Date(investible.last_investment_at);
   return investible;
-};
+}
 
-const reFormatInvestibles = (items) => {
-  Object.keys(items).forEach((key) => {
-    items[key].forEach((investible) => {
-      reFormatInvestible(investible);
-    });
-  });
-  return items;
-};
+function reFormatInvestibles(investibles){
+  investibles.forEach((item) => {reFormatInvestible(item)});
+}
+
+
+function getInvestibleCreatedState(state, action){
+  const marketId = action.marketId ? action.marketId : 'template';
+  let investibles = action.investibles ? action.investibles : action.investible;
+  if (!Array.isArray(investibles)) {
+    investibles = [investibles];
+  }
+  reFormatInvestibles(investibles);
+  const newState = { ...state };
+  // console.log(`Combining ${JSON.stringify(investibles)}`);
+  newState[marketId] = _.unionBy(investibles, state[marketId], 'id');
+  return newState;
+}
+
+function getMarketInvestibleCreatedState(state, action){
+  const newState = { ...state };
+  const { investment, marketInvestible } = action;
+  const investibleId = marketInvestible ? marketInvestible.investible_id
+    : investment.investible_id;
+  const findInvestibleId = marketInvestible ? marketInvestible.copiedInvestibleId
+    : investibleId;
+  const investibleMarketId = marketInvestible ? 'template' : investment.market_id;
+  const investible = state[investibleMarketId].find(element => element.id === findInvestibleId);
+  let investibleCopy;
+  if (marketInvestible) {
+    // This is a bind to market
+    investibleCopy = { ...investible, ...marketInvestible };
+  } else {
+    investibleCopy = { ...investible };
+  }
+  investibleCopy.id = investibleId;
+  investibleCopy.quantity = investment ? investment.investible_quantity : 0;
+  investibleCopy.current_user_investment = investment ? investment.current_user_investment : 0;
+  reFormatInvestible(investibleCopy);
+  newState[investibleCopy.market_id] = _.unionBy([investibleCopy],
+    state[investibleCopy.market_id], 'id');
+  return newState;
+}
+
+function getMarketInvestibleDeletedState(state, action){
+  const newState = { ...state };
+  newState[action.marketId] = state[action.marketId].filter(
+    item => item.id !== action.investibleId
+  );
+  return newState;
+}
 
 const items = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_INVESTIBLES:
     case INVESTIBLE_CREATED:
-      const marketId = action.marketId ? action.marketId : 'template';
-      let investibles = action.investibles ? action.investibles : action.investible;
-      if (!Array.isArray(investibles)) {
-        investibles = [investibles];
-      }
-      const newState = { ...state };
-      console.log(`Combining ${JSON.stringify(investibles)}`);
-      newState[marketId] = _.unionBy(investibles, state[marketId], 'id');
-      return newState;
+      return getInvestibleCreatedState(state, action);
     case MARKET_INVESTIBLE_DELETED:
-      const newStateForDelete = { ...state };
-      newStateForDelete[action.marketId] = state[action.marketId].filter(
-        item => item.id !== action.investibleId
-      );
-      return newStateForDelete;
+      return getMarketInvestibleDeletedState(state, action);
     case INVESTMENT_CREATED:
     case MARKET_INVESTIBLE_CREATED:
-      const newStateForCreation = { ...state };
-      const { investment, marketInvestible } = action;
-      const investibleId = marketInvestible ? marketInvestible.investible_id
-        : investment.investible_id;
-      const findInvestibleId = marketInvestible ? marketInvestible.copiedInvestibleId
-        : investibleId;
-      const investibleMarketId = marketInvestible ? 'template' : investment.market_id;
-      const investible = state[investibleMarketId].find(element => element.id === findInvestibleId);
-      let investibleCopy;
-      if (marketInvestible) {
-        // This is a bind to market
-        investibleCopy = { ...investible, ...marketInvestible };
-      } else {
-        investibleCopy = { ...investible };
-      }
-      investibleCopy.id = investibleId;
-      investibleCopy.quantity = investment ? investment.investible_quantity : 0;
-      investibleCopy.current_user_investment = investment ? investment.current_user_investment : 0;
-      newStateForCreation[investibleCopy.market_id] = _.unionBy([investibleCopy],
-        state[investibleCopy.market_id], 'id');
-      return newStateForCreation;
+      return getMarketInvestibleCreatedState(state, action);
     default:
       return state;
   }
 };
 
-export const getInvestibles = state => reFormatInvestibles(state.items);
+export const getInvestibles = state => state.items;
 
 export default combineReducers({
   items,

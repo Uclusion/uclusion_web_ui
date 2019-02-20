fc477e79ebe86a7829ccf0bb6b4e3c79f28771ce
Benjamin Follis <bfollis@uclusion.com>
2019-02-19 19:23:46 -0800

    new version of search, and removed setting the redux current market from the withMarketId since it was causing infinite loops

--- src/components/Investibles/InvestibleSearchBox.js
+++ src/components/Investibles/InvestibleSearchBox.js
@@ -1,62 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import elasticlunr from 'elasticlunr';
 import { TextField } from '@material-ui/core';
 
 import { updateSearchResults } from '../../store/Search/actions';
-import { getCurrentMarketId } from '../../store/Markets/reducer';
-import { getActiveInvestibleSearchQuery } from '../../store/Search/reducer';
-import { getInvestibles } from '../../store/MarketInvestibles/reducer';
+
+import { getActiveInvestibleSearches, getSerializedIndexes } from '../../store/Search/reducer';
+import { withMarketId } from '../PathProps/MarketId';
 
 function InvestibleSearchBox(props) {
-  const { dispatch, currentMarketId, marketInvestibles, query } = props;
-  const [index, setIndex] = useState({});
-  const [oldMarketId, setOldMarketId] = useState('');
-
-  function removeHtml(html){
-    const doc = new DOMParser().parseFromString(html, 'text/html');
-    return doc.body.textContent || '';
-  }
-
-  useEffect(() => {
-    if (oldMarketId !== currentMarketId) {
-      const newIndex = elasticlunr(function () {
-        this.addField('name');
-        this.addField('description');
-        this.setRef('id');
-        this.saveDocument(false);
-      });
-      if (marketInvestibles) {
-        marketInvestibles.forEach((investible) => {
-          const doc = { ...investible };
-          doc.description = removeHtml(doc.description);
-          newIndex.addDoc(doc);
-        });
-      }
-      setIndex(newIndex);
-      setOldMarketId(currentMarketId);
-    }
-  });
+  const { dispatch, marketId, query, serializedIndexes, searches} = props;
+  const marketSearch = searches[marketId];
+  const marketSearchQuery = marketSearch? marketSearch.query : '';
 
   function doSearch(newQuery){
-    const results = index.search(newQuery, { expand: true });
-    dispatch(updateSearchResults(newQuery, results));
+    const serializedIndex = serializedIndexes[marketId];
+    // if we don't have an index, there's nothing to search against
+    if (serializedIndex) {
+      const index = elasticlunr.Index.load(serializedIndex);
+      const results = index.search(newQuery, { expand: true });
+      dispatch(updateSearchResults(newQuery, results, marketId));
+    }
   }
 
   return (
-    <TextField value={query} onChange={event => doSearch(event.target.value)}/>
+    <TextField value={marketSearchQuery} onChange={event => doSearch(event.target.value)}/>
   );
 }
 
 function mapStateToProps(state) {
-  const currentInvestibles = getInvestibles(state.investiblesReducer);
-  const currentMarket = getCurrentMarketId(state.marketsReducer);
-  const marketInvestibles = currentInvestibles[currentMarket];
-  const query = getActiveInvestibleSearchQuery(state.searchReducer);
+  const searches = getActiveInvestibleSearches(state.searchReducer);
+  const serializedIndexes = getSerializedIndexes(state.searchReducer);
+
   return {
-    currentMarket,
-    marketInvestibles,
-    query,
+    searches,
+    serializedIndexes,
   };
 }
 
@@ -64,4 +42,4 @@ function mapDispatchToProps(dispatch) {
   return { dispatch };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(InvestibleSearchBox);
+export default connect(mapStateToProps, mapDispatchToProps)(withMarketId(InvestibleSearchBox));

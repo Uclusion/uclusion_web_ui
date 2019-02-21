7e5c5c73188f07c7ee70b027967103d83dab6b2b
Ben Follis <bfollis@uclusion.com>
2019-02-20 20:27:33 -0800

    Removed searchfield since it didn't work, and split the search reducer into the active search and search indexes reducer

---
+++ src/store/ActiveSearches/reducer.js
@@ -0,0 +1,20 @@
+import { combineReducers } from "redux";
+import { INVESTIBLE_SEARCH_RESULTS } from './actions';
+
+
+function investibleSearches(state = {}, action) {
+  const newState = { ...state };
+  switch (action.type) {
+    case INVESTIBLE_SEARCH_RESULTS:
+      newState[action.marketId] = { query: action.query, results: action.results};
+      return newState;
+    default:
+      return newState;
+  }
+}
+export function getActiveInvestibleSearches(state){
+  return state.investibleSearches;
+}
+
+
+export default combineReducers({ investibleSearches });

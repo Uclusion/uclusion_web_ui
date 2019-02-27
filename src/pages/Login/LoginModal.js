6c4e64d91769f5ed5fe6e747134517750a941611
devwiz1028 <devwiz.sh@gmail.com>
2019-02-27 16:00:53 -0400

    cognito login change password

--- src/pages/Login/LoginModal.js
+++ src/pages/Login/LoginModal.js
@@ -32,11 +32,18 @@ const styles = theme => ({
   },
 });
 
+let cognitoAuthorizer = null;
+
 function LoginModal(props) {
   const [allowGuestLogin, setAllowGuestLogin] = useState(false);
   const [allowCognitoLogin, setAllowCognitoLogin] = useState(false);
+  const [allowChangePassword, setAllowChangePassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  ValidatorForm.addValidationRule('isPasswordMatch', value => (value === newPassword));
+
   function getDestinationPage(subPath, includeAuthMarket) {
     const currentPage = new URL(window.location.href);
     let authMarketId;
@@ -52,13 +59,8 @@ function LoginModal(props) {
   function getLoginParams() {
     const marketId = getAuthMarketId();
     const parsed = new URL(window.location.href);
-    let page = 'investibles';
-    if (parsed.search.includes('destinationPage')) {
-      page = parsed.search.split('=')[1];
-      if (page.includes('&')) {
-        page = page.split('&')[0];
-      }
-    }
+    const page = parsed.searchParams.get('destinationPage') || 'investibles';
+    const newLogin = parsed.searchParams.get('newLogin');
     const destinationPage = getDestinationPage(page, true);
     const redirectUrl = getDestinationPage('post_auth', false);
     const pageUrl = window.location.href;
@@ -72,6 +74,7 @@ function LoginModal(props) {
       redirectUrl,
       pageUrl,
       uclusionUrl,
+      newLogin,
     };
   }
   useEffect(() => {
@@ -104,23 +107,43 @@ function LoginModal(props) {
     doLoginRedirect(authorizer, loginParams);
   }
 
-  function loginCognito() {
+  function cognitoTokenGenerated() {
     const { destinationPage } = getLoginParams();
+    const authInfo = {
+      token: cognitoAuthorizer.storedToken,
+      type: 'cognito',
+    };
+    setUclusionLocalStorageItem('auth', authInfo);
+    window.location = destinationPage;
+  }
+
+  function changePasswordCognito() {
+    cognitoAuthorizer.changePassword(newPassword)
+      .then(() => {
+        cognitoTokenGenerated();
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('Cannot change password');
+        setAllowChangePassword(false);
+      });
+  }
+
+  function loginCognito() {
+    const { newLogin } = getLoginParams();
     const authorizerConfiguration = {
       username: email,
       password,
       poolId: 'us-west-2_Z3vZuhzd2',
       clientId: '2off68ct2ntku805jt7sip0j1b',
     };
-
-    const authorizer = new CognitoAuthorizer(authorizerConfiguration);
-    authorizer.authorize().then((token) => {
-      const authInfo = {
-        token,
-        type: 'cognito',
-      };
-      setUclusionLocalStorageItem('auth', authInfo);
-      window.location = destinationPage;
+    cognitoAuthorizer = new CognitoAuthorizer(authorizerConfiguration);
+    cognitoAuthorizer.authorize().then(() => {
+      if (newLogin === 'true') {
+        setAllowChangePassword(true);
+      } else {
+        cognitoTokenGenerated();
+      }
     }).catch((error) => {
       console.log(error);
       alert('Cannot signin');
@@ -143,33 +166,35 @@ function LoginModal(props) {
   const { intl, classes, ...other } = props;
   return (
     <Dialog onClose={() => null} aria-labelledby="simple-dialog-title" {...other}>
-      <DialogTitle id="simple-dialog-title">Log In</DialogTitle>
+      <DialogTitle id="simple-dialog-title">
+        {allowChangePassword ? 'Change Password' : 'Log In'}
+      </DialogTitle>
       <div>
-        <List>
-          {allowCognitoLogin
-          && (
+        {allowChangePassword ? (
+          <List>
             <ListItem>
-              <ValidatorForm onSubmit={loginCognito}>
+              <ValidatorForm onSubmit={changePasswordCognito}>
                 <TextValidator
                   className={classes.input}
-                  label="Email"
-                  name="email"
-                  validators={['required', 'isEmail']}
-                  errorMessages={['Email is required', 'Email is not valid']}
-                  fullWidth
-                  value={email}
-                  onChange={event => setEmail(event.target.value)}
-                />
-                <TextValidator
-                  className={classes.input}
-                  label="Password"
-                  name="password"
+                  label="New Password"
+                  name="new_password"
                   type="password"
                   validators={['required']}
                   errorMessages={['Password is required']}
                   fullWidth
-                  value={password}
-                  onChange={event => setPassword(event.target.value)}
+                  value={newPassword}
+                  onChange={event => setNewPassword(event.target.value)}
+                />
+                <TextValidator
+                  className={classes.input}
+                  label="Confirm Password"
+                  name="confirm_password"
+                  type="password"
+                  validators={['isPasswordMatch', 'required']}
+                  errorMessages={['Passwords do not match', 'Password is required']}
+                  fullWidth
+                  value={confirmPassword}
+                  onChange={event => setConfirmPassword(event.target.value)}
                 />
                 <Button
                   className={classes.button}
@@ -177,45 +202,84 @@ function LoginModal(props) {
                   variant="contained"
                   color="primary"
                 >
-                  Login Cognito
+                  Change Password
                 </Button>
               </ValidatorForm>
             </ListItem>
-          )}
-          <ListItem>
-            <Button
-              className={classes.button}
-              variant="contained"
-              color="primary"
-              onClick={loginOidc}
-            >
-              {intl.formatMessage({ id: 'login_admin' })}
-            </Button>
-          </ListItem>
-          <ListItem>
-            <Button
-              className={classes.button}
-              variant="contained"
-              color="primary"
-              onClick={loginSso}
-            >
-              {intl.formatMessage({ id: 'login_user' })}
-            </Button>
-          </ListItem>
-          {allowGuestLogin
-          && (
+          </List>
+        ) : (
+          <List>
+            {allowCognitoLogin
+            && (
+              <ListItem>
+                <ValidatorForm onSubmit={loginCognito}>
+                  <TextValidator
+                    className={classes.input}
+                    label="Email"
+                    name="email"
+                    validators={['required', 'isEmail']}
+                    errorMessages={['Email is required', 'Email is not valid']}
+                    fullWidth
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                  />
+                  <TextValidator
+                    className={classes.input}
+                    label="Password"
+                    name="password"
+                    type="password"
+                    validators={['required']}
+                    errorMessages={['Password is required']}
+                    fullWidth
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                  />
+                  <Button
+                    className={classes.button}
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                  >
+                    Login Cognito
+                  </Button>
+                </ValidatorForm>
+              </ListItem>
+            )}
             <ListItem>
               <Button
                 className={classes.button}
                 variant="contained"
                 color="primary"
-                onClick={loginAnonymous}
+                onClick={loginOidc}
               >
-                {intl.formatMessage({ id: 'login_guest' })}
+                {intl.formatMessage({ id: 'login_admin' })}
               </Button>
             </ListItem>
-          )}
-        </List>
+            <ListItem>
+              <Button
+                className={classes.button}
+                variant="contained"
+                color="primary"
+                onClick={loginSso}
+              >
+                {intl.formatMessage({ id: 'login_user' })}
+              </Button>
+            </ListItem>
+            {allowGuestLogin
+            && (
+              <ListItem>
+                <Button
+                  className={classes.button}
+                  variant="contained"
+                  color="primary"
+                  onClick={loginAnonymous}
+                >
+                  {intl.formatMessage({ id: 'login_guest' })}
+                </Button>
+              </ListItem>
+            )}
+          </List>
+        )}
       </div>
     </Dialog>
   );

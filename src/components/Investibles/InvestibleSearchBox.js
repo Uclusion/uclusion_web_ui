55a8e98aca51d67335de9aec04211a9d73cba519
devwiz1028 <devwiz.sh@gmail.com>
2019-02-28 01:02:37 -0400

    investible search box styling

--- src/components/Investibles/InvestibleSearchBox.js
+++ src/components/Investibles/InvestibleSearchBox.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withTheme, withStyles } from '@material-ui/core/styles';
 import elasticlunr from 'elasticlunr';
-import { TextField } from '@material-ui/core';
+import {
+  FormControl,
+  Input,
+  InputLabel,
+  InputAdornment,
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 import { updateSearchResults } from '../../store/ActiveSearches/actions';
 
@@ -9,12 +16,27 @@ import { getActiveInvestibleSearches } from '../../store/ActiveSearches/reducer'
 import { getSerializedMarketIndexes } from '../../store/SearchIndexes/reducer';
 import { withMarketId } from '../PathProps/MarketId';
 
-function InvestibleSearchBox(props) {
-  const { dispatch, marketId, serializedIndexes, searches} = props;
-  const marketSearch = searches[marketId];
-  const marketSearchQuery = marketSearch? marketSearch.query : '';
+const styles = theme => ({
+  root: {
+    margin: theme.spacing.unit,
+    marginTop: theme.spacing.unit * 2,
+    maxWidth: 384,
+  },
+});
 
-  function doSearch(newQuery){
+function InvestibleSearchBox(props) {
+  const {
+    dispatch,
+    marketId,
+    serializedIndexes,
+    searches,
+    classes,
+  } = props;
+
+  const marketSearch = searches[marketId];
+  const marketSearchQuery = marketSearch ? marketSearch.query : '';
+
+  function doSearch(newQuery) {
     const serializedIndex = serializedIndexes[marketId];
     // if we don't have an index, there's nothing to search against
     if (serializedIndex) {
@@ -25,7 +47,20 @@ function InvestibleSearchBox(props) {
   }
 
   return (
-    <TextField value={marketSearchQuery} onChange={event => doSearch(event.target.value)}/>
+    <FormControl className={classes.root}>
+      <InputLabel htmlFor="adornment-search" shrink>Search Investibles</InputLabel>
+      <Input
+        id="adornment-search"
+        type="text"
+        value={marketSearchQuery}
+        onChange={event => doSearch(event.target.value)}
+        endAdornment={(
+          <InputAdornment position="end">
+            <SearchIcon />
+          </InputAdornment>
+        )}
+      />
+    </FormControl>
   );
 }
 
@@ -43,4 +78,4 @@ function mapDispatchToProps(dispatch) {
   return { dispatch };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withMarketId(InvestibleSearchBox));
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(withTheme()(withMarketId(InvestibleSearchBox))));

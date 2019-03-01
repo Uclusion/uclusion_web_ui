770692478dd047b6d7c71aad5e989482a7bd7519
disrael <disrael@uclusion.com>
2019-03-01 08:37:14 -0800

    Avoid dispatch in render

--- src/components/Investibles/InvestibleSearchBox.js
+++ src/components/Investibles/InvestibleSearchBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { withTheme, withStyles } from '@material-ui/core/styles';
 import elasticlunr from 'elasticlunr';
@@ -9,9 +9,7 @@ import {
   InputAdornment,
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-
 import { updateSearchResults } from '../../store/ActiveSearches/actions';
-
 import { getActiveInvestibleSearches } from '../../store/ActiveSearches/reducer';
 import { getSerializedMarketIndexes } from '../../store/SearchIndexes/reducer';
 import { withMarketId } from '../PathProps/MarketId';
@@ -25,6 +23,7 @@ const styles = theme => ({
 });
 
 function InvestibleSearchBox(props) {
+  const [searchQuery, setSearchQuery] = useState(undefined);
   const {
     dispatch,
     marketId,
@@ -37,19 +36,26 @@ function InvestibleSearchBox(props) {
   const marketSearchQuery = marketSearch ? marketSearch.query : '';
 
   function doSearch(newQuery) {
-    const serializedIndex = serializedIndexes[marketId];
-    // if we don't have an index, there's nothing to search against
-    if (serializedIndex) {
-      const index = elasticlunr.Index.load(JSON.parse(serializedIndex));
-      // Without this working around "we" returns nothing but "web" returns things - see https://github.com/olivernn/lunr.js/issues/38
-      // However in that bug they removed stemmer and I am removing stopWordFilter
-      // BUT STOP WORDS ARE STILL IN EFFECT
-      // on the search results - this just removes the processing of stop words from the input
-      index.pipeline._queue = index.pipeline._queue.filter(lunrPipeLineFunction => lunrPipeLineFunction.label !== 'stopWordFilter');
-      const results = index.search(newQuery, { expand: true });
-      dispatch(updateSearchResults(newQuery, results, marketId));
-    }
+    setSearchQuery(newQuery);
   }
+
+  useEffect(() => {
+    if (marketId && searchQuery !== undefined) {
+      const serializedIndex = serializedIndexes[marketId];
+      // if we don't have an index, there's nothing to search against
+      if (serializedIndex) {
+        const index = elasticlunr.Index.load(JSON.parse(serializedIndex));
+        // Without this working around "we" returns nothing but "web" returns things - see https://github.com/olivernn/lunr.js/issues/38
+        // However in that bug they removed stemmer and I am removing stopWordFilter
+        // BUT STOP WORDS ARE STILL IN EFFECT
+        // on the search results - this just removes the processing of stop words from the input
+        index.pipeline._queue = index.pipeline._queue.filter(lunrPipeLineFunction => lunrPipeLineFunction.label !== 'stopWordFilter');
+        const results = index.search(searchQuery, { expand: true });
+        dispatch(updateSearchResults(searchQuery, results, marketId));
+      }
+    }
+    return () => {};
+  }, [marketId, searchQuery]);
 
   return (
     <FormControl className={classes.root}>

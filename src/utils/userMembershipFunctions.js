a9aecda5859ba108d0ebe8b691206907cb2a79f7
disrael <disrael@uclusion.com>
2019-05-06 15:22:06 -0700

    fix: problem on multiple users on team memberships

--- src/utils/userMembershipFunctions.js
+++ src/utils/userMembershipFunctions.js
@@ -3,7 +3,7 @@ import { getClient } from '../config/uclusionClient';
 
 export function processUserForDisplay(user, marketId) {
   const processed = { ...user };
-  const marketPresence = user.market_presences.find(presence => presence.market_id === marketId);
+  const marketPresence = user.market_presences.find(presence => presence.market_id === marketId || presence.id === marketId);
   processed.quantity = marketPresence.quantity;
   processed.quantity_invested = marketPresence.quantity_invested;
   return processed;

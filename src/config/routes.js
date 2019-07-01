5e383913eee036ae9b0f9434cca1495a3fe34bdf
Ben Follis <bfollis@uclusion.com>
2019-06-30 22:23:24 -0700

    Feat: Removed categories, as we don't support them any more

--- src/config/routes.js
+++ src/config/routes.js
@@ -11,7 +11,6 @@ const AsyncPostAuth = MyLoadable({ loader: () => import('../pages/Login/PostAuth
 const AsyncInvite = MyLoadable({ loader: () => import('../pages/Invite/Invite') });
 const AsyncTeams = MyLoadable({ loader: () => import('../pages/TeamMemberships/UserMemberships') });
 const AsyncMarket = MyLoadable({ loader: () => import('../pages/Market/MarketManagement') });
-const AsyncCategories = MyLoadable({ loader: () => import('../pages/Categories/CategoryList') });
 const AsyncNewCognito = MyLoadable({ loader: () => import('../pages/Cognito/NewCognito') });
 const AsyncAbout = MyLoadable({ loader: () => import('../pages/About/About') });
 const AsyncLogin = MyLoadable({ loader: () => import('../pages/Login/Login') });
@@ -19,7 +18,6 @@ const AsyncLogin = MyLoadable({ loader: () => import('../pages/Login/Login') });
 const routes = [
   <Route type="public" path="/:marketId/dashboard" exact component={AsyncDashboard} />,
   <Route type="public" path="/:marketId/investibles" exact component={AsyncInvestibles} />,
-  <Route type="public" path="/:marketId/marketCategories" exact component={AsyncCategories} />,
   <Route type="public" path="/:marketId/teams" exact component={AsyncTeams} />,
   <Route type="public" path="/:marketId/market" exact component={AsyncMarket} />,
   <Route type="public" path="/:marketId/invite" exact component={AsyncInvite} />,

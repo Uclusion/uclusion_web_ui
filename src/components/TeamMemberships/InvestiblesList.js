a98013a6156e90ceb8ac9219c26bea65fa7e53dd
Ben Follis <bfollis@uclusion.com>
2019-06-30 22:31:13 -0700

    Fix: removed duplicate investibles list item

--- src/components/TeamMemberships/InvestiblesList.js
+++ src/components/TeamMemberships/InvestiblesList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import InvestiblesListItem from './InvestiblesListItem';
+import InvestiblesListItem from '../Investibles/InvestibleListItem';
 
 function InvestiblesList(props) {
   const { investibles } = props;

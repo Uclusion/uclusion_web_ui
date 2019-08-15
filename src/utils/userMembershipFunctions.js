4bc383c369880aca3a207d27ee2cc975fe229697
Benjamin Follis <bfollis@uclusion.com>
2019-08-15 14:06:10 -0700

    Removed unused files

--- src/utils/userMembershipFunctions.js
+++
@@ -1,7 +0,0 @@
-export function processUserForDisplay(user, marketId) {
-  const processed = { ...user };
-  const marketPresence = user.market_presences.find(presence => presence.market_id === marketId || presence.id === marketId);
-  processed.quantity = marketPresence.quantity;
-  processed.quantity_invested = marketPresence.quantity_invested;
-  return processed;
-}
--- src/utils/userStateFunctions.js
+++
@@ -1,4 +0,0 @@
-
-export function clearUserState(dispatch) {
-  localStorage.clear();
-}

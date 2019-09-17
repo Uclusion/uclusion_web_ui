fc0c4a93172df265fb77002d42f6f2eab3b60881
Ben Follis <bfollis@uclusion.com>
2019-09-16 17:39:50 -0700

    File Upload experient part 3

--- src/api/files.js
+++ src/api/files.js
@@ -5,38 +5,22 @@
  */
 import { getMarketClient } from './uclusionClient';
 
-function getContentPromise(file) {
-  const reader = new FileReader();
-  return new Promise((resolve, reject) => {
-    const loadHandler = reader.addEventListener('load', () => {
-      resolve(reader.result);
-    });
-    const errorHandler = reader.addEventListener('error', () => {
-      reject(reader.error);
-    });
-    reader.readAsBinaryString(file);
-  });
-}
-
 export function uploadFileToS3(marketId, file) {
-  console.log(file);
-  return getContentPromise(file)
-    .then((buffer) => {
-      return getMarketClient(marketId)
-        .then(client => client.investibles.createFileUploadDestination(file.type, file.size))
-        .then((destination) => {
-          const { url, fields } = destination;
-          // load up the fields and file data into the post body
-          const body = new FormData();
-          for (const [field, value] of Object.entries(fields)) {
-            body.append(field, value);
-          }
-          // aws ignores all fields after the file field, so the data has to be last
-          body.append('file', file);
-          const fetchParams = { method: 'POST', body };
-          return fetch(url, fetchParams)
-            .then(() => url); // just want to give back the successful url
-        });
+  const { type, size } = file;
+  return getMarketClient(marketId)
+    .then(client => client.investibles.createFileUploadDestination(type, size))
+    .then((destination) => {
+      const { url, fields } = destination;
+      // load up the fields and file data into the post body
+      const body = new FormData();
+      for (const [field, value] of Object.entries(fields)) {
+        body.append(field, value);
+      }
+      // aws ignores all fields after the file field, so the data has to be last
+      body.append('file', file);
+      const fetchParams = { method: 'POST', body };
+      return fetch(url, fetchParams)
+        .then(() => url); // just want to give back the successful url
     });
 }
 

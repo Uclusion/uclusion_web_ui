0414447b36e5c7c3f84678b9e52b9e2132d10260
disrael <disrael@uclusion.com>
2019-10-02 08:07:48 -0700

    fix: Switch form to match recommended in https://reacttraining.com/blog/react-router-v5-1 but still not preserving state

--- src/containers/Root/Root.js
+++ src/containers/Root/Root.js
@@ -1,32 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import { Router, Route, Switch } from 'react-router-dom';
-import AppLayout from '../AppLayout';
+import { withStyles } from '@material-ui/core';
+import { Route, Router, Switch } from 'react-router-dom';
 import { defaultTheme } from '../../config/themes';
+import Drawer from '../Drawer';
+import Markets from '../../pages/DecisionDialogs/Markets';
+import Notifications from '../../pages/ActionCenter/Notifications';
+import Market from '../../pages/DecisionDialog/Market';
+import About from '../../pages/About/About';
+import PageNotFound from '../../pages/PageNotFound/PageNotFound';
 
 // eslint-disable-next-line
 const history = require('history').createBrowserHistory();
 
-function Root(props){
+const styles = {
+  body: {
+    height: '100%',
+  },
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+};
 
-  const { appConfig } = props;
+function Root(props) {
+  const { classes, appConfig } = props;
 
   const theme = defaultTheme;
   return (
     <MuiThemeProvider theme={theme}>
       <Router history={history}>
-        <Switch>
-          <Route children={props => <AppLayout {...props} appConfig={appConfig}/>} />
-        </Switch>
+        <div className={classes.body}>
+          <div className={classes.root}>
+            <Drawer appConfig={appConfig} />
+            <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+              <Switch>
+                <Route type="public" path="/dialogs" exact render={Markets} />
+                <Route type="public" path="/notifications" exact>
+                  <Notifications />
+                </Route>
+                <Route type="public" path="/:marketId" exact>
+                  <Market />
+                </Route>
+                <Route type="public" path="/:marketId/about" exact>
+                  <About />
+                </Route>
+                <Route>
+                  <PageNotFound />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
       </Router>
     </MuiThemeProvider>
   );
-
 }
 
 Root.propTypes = {
   appConfig: PropTypes.object.isRequired, // eslint-disable-line
 };
 
-export default Root;
+export default withStyles(styles)(Root);

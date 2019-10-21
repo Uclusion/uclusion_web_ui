522e4557c22fc0d2d16265ef905d0935d2c1409e
disrael <disrael@uclusion.com>
2019-10-21 11:26:16 -0700

    fix: do not async load main

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Loadable from 'react-loadable';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import A2HSProvider from 'a2hs';
-import LoadingComponent from './components/LoadingComponent';
 import Main from './containers/Main';
-
-const MainAsync = Loadable({
-  loader: () => import('./containers/Main'),
-  loading: () => <LoadingComponent />,
-});
 
 ReactDOM.render(
   <A2HSProvider>
     <Router>
-      <Route component={Main} />
+      <Route>
+        <Main />
+      </Route>
     </Router>
   </A2HSProvider>,
-  document.getElementById('root'),
-  () => {
-    setTimeout(() => {
-      MainAsync.preload();
-    }, 1500);
-  },
+  document.getElementById('root')
 );

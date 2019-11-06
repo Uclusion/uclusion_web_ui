b8583e389eec6c1c50aafde7d1684084d898094b
disrael <disrael@uclusion.com>
2019-11-05 22:52:56 -0800

    feat: versions - partial checkin

--- src/contexts/InvestibesContext/investiblesContextMessages.js
+++ src/contexts/InvestibesContext/investiblesContextMessages.js
@@ -1,22 +1,23 @@
 import { Hub } from '@aws-amplify/core';
-
 import { refreshInvestibles } from './investiblesContextHelper';
+import {
+  PUSH_INVESTIBLES_CHANNEL,
+  VERSIONS_EVENT,
+} from '../VersionsContext/versionsContextHelper';
 
 function beginListening(dispatch) {
-  /* Hub.listen(PUSH_INVESTIBLES_CHANNEL, (data) => {
-   const { payload: { event, message } } = data;
+  Hub.listen(PUSH_INVESTIBLES_CHANNEL, (data) => {
+    const { payload: { event, message } } = data;
 
-   switch (event) {
-   case MESSAGES_EVENT: {
-       const { indirect_object_id: marketId } = message;
-       refreshInvestibles(dispatch, marketId);
-       break;
-     }
+    switch (event) {
+      case VERSIONS_EVENT: {
+        message.map((marketId) => refreshInvestibles(dispatch, marketId));
+        break;
+      }
       default:
         console.debug(`Ignoring push event ${event}`);
     }
   });
-*/
 }
 
 export default beginListening;

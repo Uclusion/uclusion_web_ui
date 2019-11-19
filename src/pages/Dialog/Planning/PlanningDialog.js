0d63f71525cba347a5e3c73f0173ccb47f0329a5
disrael <disrael@uclusion.com>
2019-11-19 09:05:07 -0800

    fix: planning dialog by user start

--- src/pages/Dialog/Planning/PlanningDialog.js
+++ src/pages/Dialog/Planning/PlanningDialog.js
@@ -5,16 +5,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Summary from '../Summary';
 import ProposedIdeas from '../Decision/ProposedIdeas';
+import { Typography } from '@material-ui/core';
 
 function PlanningDialog(props) {
-
-  const { market, investibles } = props;
+  const { market, investibles, marketPresences } = props;
   const { id: marketId } = market;
+
+  function getInvestiblesByPerson(investibles, marketPresences) {
+    const followingPresences = marketPresences.filter((presence) => presence.following);
+    return (
+      <>
+        {
+        followingPresences.map((presence) => {
+          const myInvestibles = investibles.filter((investible) => {
+            const marketInfo = investible.market_infos.find((info) => info.market_id === marketId);
+            return marketInfo.assigned.includes(presence.id);
+          });
+          return (
+            <>
+              <br />
+              <Typography
+                noWrap
+              >
+                {presence.name}
+              </Typography>
+              <br />
+              <ProposedIdeas investibles={myInvestibles} marketId={marketId} />
+            </>
+          );
+        })
+      }
+      </>
+    );
+  }
 
   return (
     <div>
       <Summary market={market} />
-      {investibles && <ProposedIdeas investibles={investibles} marketId={marketId} />}
+      {getInvestiblesByPerson(investibles, marketPresences)}
     </div>
   );
 }
@@ -24,10 +52,13 @@ PlanningDialog.propTypes = {
   market: PropTypes.object.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
   investibles: PropTypes.arrayOf(PropTypes.object),
+  // eslint-disable-next-line react/forbid-prop-types
+  marketPresences: PropTypes.arrayOf(PropTypes.object),
 };
 
 PlanningDialog.defaultProps = {
   investibles: [],
+  marketPresences: [],
 };
 
 export default PlanningDialog;

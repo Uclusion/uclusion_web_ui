c44fcb2df995719f2bfb2569f9c9170e073fef8d
disrael <disrael@uclusion.com>
2019-12-07 06:43:08 -0800

    feat: homePlanningReport

--- src/config/locales/en.js
+++ src/config/locales/en.js
@@ -251,7 +251,7 @@ const messages = defineMessages({
   homeAddDecision: 'Create Decision',
   homeAddPlanning: 'Creat Epic Plan',
   homeAddInitiative: 'Create Initiative',
-  homePlanningReport: 'Active Story Estimates (in days)',
+  homePlanningReport: 'Active Story Value Estimates (in days)',
   homeViewArchives: 'View Archives',
 
   // Decision Dialogs

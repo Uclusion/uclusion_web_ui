dd8b551d7894b54135985141c7ffba5fbfbbe32f
disrael <disrael@uclusion.com>
2019-12-08 22:28:16 -0800

    fix: correctly delete not fully voted notifications

--- src/api/users.js
+++ src/api/users.js
@@ -2,7 +2,7 @@ import { getMarketClient } from './uclusionClient';
 
 export function deleteMessage(message) {
   const { marketId, type_object_id: typeObjectId, aType } = message;
-  const objectId = typeObjectId.split('_')[1];
+  const objectId = typeObjectId.split('_').pop();
   return getMarketClient(marketId)
     .then((client) => client.users.removeNotification(objectId, aType));
 }

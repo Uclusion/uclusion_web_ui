a3fd5480b96de43cb5548c36d0d0ac53348b3dc6
songh666 <songhc666@gmail.com>
2019-12-28 02:05:42 -0800

    Update the color of chart bars

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,5 +1,5 @@
-export const SECTION_TYPE_PRIMARY = "primary";
-export const SECTION_TYPE_SECONDARY = "secondary";
+export const SECTION_TYPE_PRIMARY = 'primary';
+export const SECTION_TYPE_SECONDARY = 'secondary';
 
 export const DRAWER_WIDTH_CLOSED = 97;
 export const DRAWER_WIDTH_OPENED = 256;
@@ -7,5 +7,3 @@ export const MAX_FONTSIZE = 24;
 export const MIN_FONTSIZE = 12;
 export const CHART_WIDTH = 70;
 export const CHART_HEIGHT = 45;
-export const CHART_BAR_COLOR_TOP = 0xf5270f;
-export const CHART_BAR_COLOR_BOTTOM = 0x5ed635;

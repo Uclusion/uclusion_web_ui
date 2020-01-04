5c5fa93b99f7eaded65904ff3e244d2013112981
disrael <disrael@uclusion.com>
2020-01-03 16:22:08 -0800

    fix: remove compile warning

--- src/pages/Dialog/Planning/InvestibleAdd.js
+++ src/pages/Dialog/Planning/InvestibleAdd.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import {

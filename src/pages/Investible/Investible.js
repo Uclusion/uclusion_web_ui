6040f75ee8a71fa6b4d8846d786ef7b072c9b9c1
disrael <disrael@uclusion.com>
2020-01-07 15:22:28 -0800

    fix: new url for investible edit

--- src/pages/Investible/Investible.js
+++ src/pages/Investible/Investible.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router';
 import _ from 'lodash';

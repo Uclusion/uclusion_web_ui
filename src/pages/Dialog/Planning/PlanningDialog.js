aa569a0a162a8c25ea6c4d6e7ab7a2d7bd4e1a55
songh666 <songhc666@gmail.com>
2020-01-10 02:33:18 -0800

    comment box dispaly edit under comments

--- src/pages/Dialog/Planning/PlanningDialog.js
+++ src/pages/Dialog/Planning/PlanningDialog.js
@@ -265,6 +265,7 @@ function PlanningDialog(props) {
             type={SECTION_TYPE_SECONDARY}
             title={intl.formatMessage({ id: 'planningDialogDiscussionLabel' })}
           >
+            <CommentBox comments={marketComments} marketId={marketId} />
             {!commentAddHidden && (
               <CommentAddBox
                 hidden={commentAddHidden}
@@ -275,7 +276,6 @@ function PlanningDialog(props) {
                 onCancel={closeCommentAddBox}
               />
             )}
-            <CommentBox comments={marketComments} marketId={marketId} />
           </SubSection>
         </Grid>
       </Grid>

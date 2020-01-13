33cf6e7e81f94fae22386f82235986670bf4de47
songh666 <songhc666@gmail.com>
2020-01-13 08:17:34 -0800

    show initial comments at root only

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -8,6 +8,7 @@ import {
   Card,
   CardContent,
   CardActions,
+  Typography,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import _ from 'lodash';
@@ -17,8 +18,8 @@ import { REPLY_TYPE } from '../../constants/comments';
 import { reopenComment, resolveComment } from '../../api/comments';
 import SpinBlockingButton from '../SpinBlocking/SpinBlockingButton';
 import { OperationInProgressContext } from '../../contexts/OperationInProgressContext';
-// import { MarketPresencesContext } from '../../contexts/MarketPresencesContext/MarketPresencesContext';
-// import { getMarketPresences } from '../../contexts/MarketPresencesContext/marketPresencesHelper';
+import { MarketPresencesContext } from '../../contexts/MarketPresencesContext/MarketPresencesContext';
+import { getMarketPresences } from '../../contexts/MarketPresencesContext/marketPresencesHelper';
 import CustomChip from '../CustomChip';
 import CommentEdit from './CommentEdit';
 import { MarketsContext } from '../../contexts/MarketsContext/MarketsContext';
@@ -141,9 +142,9 @@ function Comment(props) {
   const intl = useIntl();
   const classes = useStyles();
   const { id, comment_type: commentType, created_by: createdBy } = comment;
-  // const [presencesState] = useContext(MarketPresencesContext);
-  // const presences = getMarketPresences(presencesState, marketId) || [];
-  // const commenter = presences.find(presence => presence.id === createdBy);
+  const [presencesState] = useContext(MarketPresencesContext);
+  const presences = getMarketPresences(presencesState, marketId) || [];
+  const commenter = presences.find(presence => presence.id === createdBy);
   const [marketsState] = useContext(MarketsContext);
   const user = getMyUserForMarket(marketsState, marketId) || {};
   const children = comments.filter(comment => comment.reply_id === id);
@@ -153,6 +154,13 @@ function Comment(props) {
   const [toggledOpen, setToggledOpen] = useState(false);
   const [operationRunning] = useContext(OperationInProgressContext);
   const [highlightedCommentState] = useContext(HighlightedCommentContext);
+
+  const isRoot = !comment.reply_id;
+  const expanded =
+    replyOpen ||
+    toggledOpen ||
+    (isRoot && !comment.resolved) ||
+    comment.reply_id;
 
   function getChildComments() {
     if (_.isEmpty(sortedChildren)) {
@@ -181,6 +189,7 @@ function Comment(props) {
     const initialComment = sortedChildren[0];
     return (
       <Box marginTop={4}>
+        {commenter && <Typography>{commenter.name}</Typography>}
         <ReadOnlyQuillEditor value={initialComment.body} paddingLeft={0} />
       </Box>
     );
@@ -217,13 +226,6 @@ function Comment(props) {
     setToggledOpen(!toggledOpen);
   }
 
-  const isRoot = !comment.reply_id;
-  const expanded =
-    replyOpen ||
-    toggledOpen ||
-    (isRoot && !comment.resolved) ||
-    comment.reply_id;
-
   return (
     <Card
       className={!isRoot ? classes.childContainer : getCommentHighlightStyle()}
@@ -256,7 +258,7 @@ function Comment(props) {
               onCancel={toggleEdit}
             />
           )}
-          {expanded && getInitialChildComments()}
+          {expanded && isRoot && getInitialChildComments()}
         </Box>
       </CardContent>
       {!toggledOpen && !replyOpen && (

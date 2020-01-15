d230ca8cd0544f9e09c27d2efe96b227fa9bf42e
songh666 <songhc666@gmail.com>
2020-01-15 02:36:51 -0800

    login page with modern styles

--- src/authorization/CustomSignIn.js
+++ src/authorization/CustomSignIn.js
@@ -1,78 +1,116 @@
-import React from 'react';
-import { SignIn } from 'aws-amplify-react';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { SignIn } from "aws-amplify-react";
+
+const useStyles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+});
 
 class CustomSignIn extends SignIn {
   constructor(props) {
     super(props);
-    this._validAuthStates = ['signIn', 'signedOut', 'signedUp'];
+    this._validAuthStates = ["signIn", "signedOut", "signedUp"];
   }
 
   showComponent() {
+    const { classes } = this.props;
     return (
-      <div>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="username"
-            >
-              Username
-            </label>
-            <input
-              id="username"
-              key="username"
-              name="username"
-              onChange={this.handleInputChange}
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              key="password"
-              name="password"
-              onChange={this.handleInputChange}
-              type="password"
-              placeholder="******************"
-            />
-            <p className="text-grey-dark text-xs">
-              Forgot your password?
-              {' '}
-              <button
-                type="button"
-                onClick={() => super.changeState('forgotPassword')}
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+        </div>
+        <form className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            key="username"
+            name="username"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Username"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="password"
+            key="password"
+            type="password"
+            id="password"
+            onChange={this.handleInputChange}
+            autoComplete="current-password"
+          />
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => super.signIn()}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link
+                href="#"
+                variant="body2"
+                onClick={() => super.changeState("forgotPassword")}
               >
-                Reset Password
-              </button>
-            </p>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => super.signIn()}
-            >
-              Login
-            </button>
-            <p className="text-grey-dark text-xs">
-              No Account?
-              {' '}
-              <button
-                type="button"
-                onClick={() => super.changeState('signUp')}
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href="#"
+                variant="body2"
+                onClick={() => super.changeState("signUp")}
               >
-                Create account
-              </button>
-            </p>
-          </div>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </form>
-      </div>
+      </Container>
     );
   }
 }
 
-export default CustomSignIn;
+export default withStyles(useStyles)(CustomSignIn);

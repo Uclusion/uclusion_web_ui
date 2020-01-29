ebdc5877997361fb4f042a8e2dbe7b6381ec2b63
Ben Follis <bfollis@uclusion.com>
2020-01-28 21:06:21 -0800

    Fix: Commit to checkpoint code. Some random api/404 call is still present that's making not verified fail

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
+import NoAccount from '../../pages/NoAccount/NoAccount';
 import Root from '../Root';
 import AppConfigProvider from '../../components/AppConfigProvider';
 import config from '../../config';
@@ -6,43 +8,67 @@ import { WebSocketProvider } from '../../contexts/WebSocketContext';
 import { OnlineStateProvider } from '../../contexts/OnlineStateContext';
 import { Auth } from 'aws-amplify';
 import LogRocket from 'logrocket';
+import { defaultTheme } from '../../config/themes';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 function App(props) {
 
   const { authState } = props;
   const configs = { ...config };
-  const [identified, setIdentitified] = useState(false);
-
+  const [userAttributes, setUserAttributes] = useState({});
   useEffect(() => {
-    if (authState === 'signedIn' && !identified) {
+    if (authState === 'signedIn') {
       Auth.currentAuthenticatedUser()
         .then((user) => {
           const { attributes } = user;
           const { sub, name, email } = attributes;
-          const userId = attributes['custom:user_id'];
-          LogRocket.identify((userId), {
+          const cognitoUserId = attributes['custom:user_id'];
+          const userAttributes = {
+            sub,
+            name,
+            email,
+            userId: cognitoUserId,
+          };
+          setUserAttributes(userAttributes);
+          LogRocket.identify((cognitoUserId), {
             name,
             sub,
             email,
-            userId,
+            cognitoUserId,
           });
-          setIdentitified(true);
+
         });
+    } else {
+      setUserAttributes({});
     }
     return () => {
     };
-  }, [authState, identified]);
+  }, [authState]);
 
   if (authState !== 'signedIn') {
     return <></>;
   }
 
+  const { userId, email } = userAttributes;
+  const hasAccount = !_.isEmpty(userId);
+
+  if (!hasAccount) {
+    return (
+      <OnlineStateProvider>
+        <ThemeProvider theme={defaultTheme}>
+          <NoAccount email={email}/>
+        </ThemeProvider>
+      </OnlineStateProvider>
+    );
+  }
 
   return (
     <OnlineStateProvider>
       <WebSocketProvider config={config}>
         <AppConfigProvider appConfig={configs}>
-          <Root appConfig={configs}/>
+          <ThemeProvider theme={defaultTheme}>
+            <Root appConfig={configs}/>
+          </ThemeProvider>
         </AppConfigProvider>
       </WebSocketProvider>
     </OnlineStateProvider>

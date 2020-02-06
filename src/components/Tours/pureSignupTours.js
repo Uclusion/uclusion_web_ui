8fcd1804c76545dc1597e9450c36c413c3a26b21
Ben Follis <bfollis@uclusion.com>
2020-02-06 11:39:38 -0800

    Shorter tour text

--- src/components/Tours/pureSignupTours.js
+++ src/components/Tours/pureSignupTours.js
@@ -24,13 +24,7 @@ export function pureSignupHomeSteps(variables) {
       disableBeacon: true,
       target: '#createDialog',
       title: `Welcome to Uclusion ${name}!`,
-      content: 'Lets show you how Uclusion can help make decisions',
-    },
-    {
-      disableBeacon: true,
-      target: '#createDialog',
-      title: 'Dialogs',
-      content: "To get started we'll be creating a new dialog",
+      content: "Let's see how Uclusion can help make decisions by creating a new dialog",
       onClose: () => document.getElementById('createDialog').click(),
     },
   ];
@@ -51,12 +45,12 @@ export const PURE_SIGNUP_ADD_DIALOG_STEPS = [
     disableBeacon: true,
   },
   {
-    content: "Decisions have deadlines, and to make sure you get results in a timely manner the dialog will end and expire after a certain number of days. Slide the slider to the left and right to set the number of days, up to two weeks.",
+    content: "Decisions have deadlines. Slide the slider to the left and right to set the number days until your deadline.",
     target: '#expires',
     placement: 'auto',
   },
   {
-    content: "Most of the time, one sentence isn't enough to describe the decision to be made. Enter any contextual information needed into the description. If it's just lunch, then feel free to just enter the time you want to go",
+    content: "Enter any contextual information needed into the description",
     target: '#description',
   },
 ];
@@ -64,37 +58,37 @@ export const PURE_SIGNUP_ADD_DIALOG_STEPS = [
 export const PURE_SIGNUP_ADD_DIALOG_OPTIONS_STEPS = [
   {
     title: 'Dialog Current Voting',
-    content: "You can see the newly created option in the 'Current Voting' section of the dialog. Votes will be visible in a histogram to the right of the name. The colors and position of the histogram convey the certainty, with uncertain being red and on the right and completely certain being green and on the left.",
+    content: "You can see the newly created option, and a vote summary, here.",
     target: '#option0',
     disableBeacon: true,
   },
   {
     title: 'Add Some More Options',
-    content: "A decision with just one option isn't really a decision, so add some more options, if it's lunch try adding your favorite food types. When you're done adding optons click the beacon over 'Manage Dialog' to resume the tour.",
+    content: "A decision with just one option isn't really a decision, so add some more options. When you're done, click the beacon over 'Manage Dialog' to resume the tour.",
     target: '#currentVoting',
     disableBeacon: true,
   },
   {
     title: 'Invite People',
-    content: "It's now time to invite other participants. We do that via the 'Manage Dialog' button in the sidebar.",
+    content: "You can invite the other participants via the 'Manage Dialog' button.",
     target: '#manageDialog',
     onClose: () => document.getElementById('manageDialog').click(),
   },
   {
     title: 'Congrats on your first Dialog!',
-    content: "Congratulations on creating your first decision, there's a few things we need to talk about before we let you go.",
+    content: "Before you go there's a few things we need tell you.",
     target: '#summary'
   },
   {
     title: 'Notifications',
-    content: "When Uclusion has something for you to do, we'll display a notification alert down here. Red notifications mean something is blocked and needs your attention immediately, while Yellow needs action, but nothing is immediately blocked.",
+    content: "We'll display notification alert downs here. Red notifications mean something is blocked, while Yellow needs attention.",
     target: '#notifications',
     disableBeacon: true,
   },
   {
     title: 'Your Profile',
     target: '#profileLink',
-    content: 'You can change your notification preferences, or perform profile actions by clicking on your name in the upper right of the screen. Now would be a good time to enable the Slack integration to get your notifications over Slack instead of email.',
+    content: 'You can change your profile and notification preferences here.',
     disableBeacon: true,
   }
 ];
@@ -102,18 +96,18 @@ export const PURE_SIGNUP_ADD_DIALOG_OPTIONS_STEPS = [
 export const PURE_SIGNUP_ADD_FIRST_OPTION_STEPS = [
   {
     title: 'Your New Dialog',
-    content: "The first thing a new dialog needs is the options that we want to present. When you create a dialog we'll take you directly to the add options page to create the first one.",
+    content: "When you create a dialog we'll take you directly to the add options page to create the first one.",
     target: '#name',
     disableBeacon: true,
   },
   {
     title: 'The First Option',
-    content: "Like dialogs, options need a name. This name will show in the dialog's 'Current Voting' section, so make it short, but descriptive. If we're having lunch, then add your favorite food type.",
+    content: "Options need a name that is short, but descriptive. If we're having lunch, then add your favorite food type.",
     target: "#name",
     disableBeacon: true
   },
   {
-    content: "Options will also need a detailed description so people know what their voting for. Put those details in the description field. For lunch, just put the address of the eatery you want to go to for this food type. When you're done, click 'Save'",
+    content: "The details go here in the description. When you're done, click 'Save'",
     target: '#description'
   },
 ];
@@ -121,31 +115,25 @@ export const PURE_SIGNUP_ADD_FIRST_OPTION_STEPS = [
 export const PURE_SIGNUP_ADD_PEOPLE_STEPS = [
   {
     title: 'Collaborate with Uclusion',
-    content: "Decisions need more than one person to participate. Uclusion provides several ways to add new people to your Dialog",
+    content: "Uclusion provides several ways to add new people to your Dialog",
     target: '#decisionAddressList',
     disableBeacon: true,
   },
   {
     title: 'Links',
-    content: 'The first is to copy an invite link to your clipboard, and send them a message in whatever app is most convenient. If the person (or people) you are inviting are not going to vote, but just watch the proceedings, then send them the observer link.',
+    content: 'The first is to copy an invite link to your clipboard, and send them a message. If the people you are inviting are not going to vote, then send them the observer link.',
     target: "#inviteLinker",
     disableBeacon: true,
   },
   {
     title: 'Email',
     target: '#emailInput',
-    content: "If you'd prefer we message them you can enter an email here, and click 'Invite Participant'. They'll get a nicely formatted email telling them what to do. If you want them to be an observer, check the 'Is Observer' box before you click invite.",
-    disableBeacon: true,
-  },
-  {
-    title: 'Address Book',
-    content: "Lastly, if you've used Uclusion with them before, they'll show up in your address book. Since you're just starting out, you won't have any of these",
-    target: '#addressBook',
+    content: "If you'd prefer we message them you can enter an email here, and click 'Invite Participant'. They'll get a nicely formatted email telling them what to do.",
     disableBeacon: true,
   },
   {
     title: 'Get Collaborating',
-    content: "Add whomever you want to participate in this dialog, and we'll make sure they stay up to date. When you're done adding people, click the dialog's name in the breadcrumbs to return to the dialog page",
+    content: "We'll make sure participants stay up to date. When you're done adding people, click the dialog's name in the breadcrumbs to return to the dialog.",
     target: '#marketCrumb',
     disableBeacon: true,
   }

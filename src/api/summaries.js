d1cfdbcac3fd1730fa57a348ad27b90d56f59954
Ben Follis <bfollis@uclusion.com>
2020-02-11 20:50:34 -0800

    Fix: App version check now happens on notifications

--- src/api/summaries.js
+++ src/api/summaries.js
@@ -1,6 +1,7 @@
 import uclusion from 'uclusion_sdk';
 import AmpifyIdentitySource from '../authorization/AmplifyIdentityTokenRefresher';
 import config from '../config';
+import { notifyNewApplicationVersion } from '../contexts/WebSocketContext';
 
 function getSummaryInfo() {
   return new AmpifyIdentitySource().getIdentity()
@@ -20,6 +21,16 @@ export function getNotifications() {
   return getSummaryInfo()
     .then((summaryInfo) => {
       const { summaryClient, idToken } = summaryInfo;
-      return summaryClient.notifications(idToken);
+      return summaryClient.notifications(idToken)
+        .then((notifications) => {
+          // TODO we have app verion here, which is odd, but we'll move it later
+          // For now notify here, and just pass it all along unmodified
+          const appAuditVersion = notifications.find((versionRow) => versionRow.type_object_id === 'app_version');
+          if (appAuditVersion) {
+            const { app_version: appVersion, requires_cache_clear: cacheClear } = appAuditVersion;
+            notifyNewApplicationVersion(appVersion, cacheClear);
+          }
+          return notifications;
+        });
     });
 }

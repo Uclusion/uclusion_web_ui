93ecd49eb5f7b961b37d135bd22ab7b0df823f60
Ben Follis <bfollis@uclusion.com>
2020-02-14 11:19:33 -0800

    Fix: We now short circuit things if you're part of a market

--- src/pages/Invites/MarketInvite.js
+++ src/pages/Invites/MarketInvite.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router';
+import _ from 'lodash';
 import {
   decomposeMarketPath,
   formMarketLink,
@@ -14,6 +15,8 @@ import { registerListener, removeListener } from '../../utils/MessageBusUtils';
 import { toastError } from '../../utils/userMessage';
 import queryString from 'query-string';
 import { NEW_MARKET } from '../../contexts/VersionsContext/versionsContextMessages';
+import { MarketsContext } from '../../contexts/MarketsContext/MarketsContext';
+import { getMarket } from '../../contexts/MarketsContext/marketsContextHelper';
 
 function MarketInvite(props) {
   const { hidden } = props;
@@ -24,43 +27,50 @@ function MarketInvite(props) {
   const { hash } = location;
   const { marketId } = decomposeMarketPath(pathname);
   const [myLoading, setMyLoading] = useState(true);
-
+  const [marketState] = useContext(MarketsContext);
   useEffect(() => {
     if (!hidden) {
-      registerListener(VERSIONS_HUB_CHANNEL, 'inviteListener', (data) => {
-        const { payload: { event, marketId: messageMarketId } } = data;
-        switch (event) {
-          case  NEW_MARKET: {
-            if (messageMarketId === marketId) {
-              console.log(`Redirecting us to market ${marketId}`);
-              setTimeout(() => {
-                navigate(history, formMarketLink(marketId));
-              }, 500);
+      const marketDetails = getMarket(marketState, marketId);
+      if (!_.isEmpty(marketDetails)) {
+        // if I know about the market, I must be part of it since we never leave
+        // TODO: IF we do implement leave on a market I know about fix this
+        navigate(history, formMarketLink(marketId));
+      } else {
+        registerListener(VERSIONS_HUB_CHANNEL, 'inviteListener', (data) => {
+          const { payload: { event, marketId: messageMarketId } } = data;
+          switch (event) {
+            case  NEW_MARKET: {
+              if (messageMarketId === marketId) {
+                console.log(`Redirecting us to market ${marketId}`);
+                setTimeout(() => {
+                  navigate(history, formMarketLink(marketId));
+                }, 500);
+              }
+              break;
             }
-            break;
+            default:
+              // ignore
+              break;
           }
-          default:
-            // ignore
-            break;
-        }
-      });
-      console.debug(`Logging into market ${marketId}`);
-      const values = queryString.parse(hash);
-      const { is_obs: isObserver } = values;
-      getMarketLogin(marketId, isObserver === 'true')
-        .then((result) => {
-          const { is_new_capability: loggedIntoNewMarket } = result;
-          if (!loggedIntoNewMarket) {
-            setMyLoading(false);
-            removeListener(VERSIONS_HUB_CHANNEL, 'inviteListener');
-            navigate(history, formMarketLink(marketId));
-          }
-        })
-        .catch((error) => {
-          setMyLoading(false);
-          console.error(error);
-          toastError('errorMarketFetchFailed');
         });
+        console.debug(`Logging into market ${marketId}`);
+        const values = queryString.parse(hash);
+        const { is_obs: isObserver } = values;
+        getMarketLogin(marketId, isObserver === 'true')
+          .then((result) => {
+            const { is_new_capability: loggedIntoNewMarket } = result;
+            if (!loggedIntoNewMarket) {
+              setMyLoading(false);
+              removeListener(VERSIONS_HUB_CHANNEL, 'inviteListener');
+              navigate(history, formMarketLink(marketId));
+            }
+          })
+          .catch((error) => {
+            setMyLoading(false);
+            console.error(error);
+            toastError('errorMarketFetchFailed');
+          });
+      }
     }
   }, [hidden, marketId, history, hash]);
 

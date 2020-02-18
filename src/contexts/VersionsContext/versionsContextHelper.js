f08cff7d78f5dee787ef7f3401089dd665e95ba6
Ben Follis <bfollis@uclusion.com>
2020-02-17 16:00:18 -0800

    state from disk

--- src/contexts/VersionsContext/versionsContextHelper.js
+++ src/contexts/VersionsContext/versionsContextHelper.js
@@ -1,5 +1,7 @@
 import { pushMessage } from '../../utils/MessageBusUtils';
 import { refreshGlobalVersion } from '../../api/versionedFetchUtils';
+import LocalForageHelper from '../LocalForageHelper';
+import { VERSIONS_CONTEXT_NAMESPACE } from './versionsContextReducer';
 
 export const NOTIFICATIONS_HUB_CHANNEL = 'NotificationsChannel';
 export const PUSH_CONTEXT_CHANNEL = 'MarketsChannel';
@@ -10,39 +12,39 @@ export const REMOVED_MARKETS_CHANNEL = 'RemovedMarketsChannel';
 export const PUSH_STAGE_CHANNEL = 'MarketsStagesChannel';
 export const VERSIONS_EVENT = 'version_push';
 
-
-export function getGlobalVersion(state) {
+export function getGlobalVersion (state) {
   return state.globalVersion;
 }
 
-export function getExistingMarkets(state) {
+export function getExistingMarkets (state) {
   return state.existingMarkets || [];
 }
 
-
-
-export function refreshVersions(state) {
+export function refreshVersions (state) {
   console.debug('Refreshing versions');
-  const {
-    existingMarkets,
-    globalVersion,
-  } = state;
-  return refreshGlobalVersion(globalVersion, existingMarkets);
+  const disk = new LocalForageHelper(VERSIONS_CONTEXT_NAMESPACE);
+  return disk.getState()
+    .then((state) => {
+      const {
+        existingMarkets,
+        globalVersion,
+      } = state;
+      return refreshGlobalVersion(globalVersion, existingMarkets);
+    });
 }
 
-export function refreshNotifications() {
+export function refreshNotifications () {
   // tell versions to go and get new notifications
-  pushMessage(NOTIFICATIONS_HUB_CHANNEL, {event: VERSIONS_EVENT});
+  pushMessage(NOTIFICATIONS_HUB_CHANNEL, { event: VERSIONS_EVENT });
 }
-
 
 // used by the reducer to actually process the new event
-export function refreshNotificationVersion(state, version) {
+export function refreshNotificationVersion (state, version) {
   const { notificationVersion } = state;
   processNewNotification(version, notificationVersion);
 }
 
-function processNewNotification(newNotificationVersion, notificationVersion) {
+function processNewNotification (newNotificationVersion, notificationVersion) {
   const { version: notificationVersionNumber } = notificationVersion;
   const { version: newNotificationVersionNumber } = newNotificationVersion;
   console.debug(`Refreshing notifications from ${notificationVersionNumber} to ${newNotificationVersionNumber}`);

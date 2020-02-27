11ed6cfc51034ef66070044ad7f649b05ed96aa1
disrael <disrael@gmail.com>
2020-02-27 11:38:43 -0800

    fix: warnings

--- src/pages/Investible/Planning/PlanningInvestibleEdit.js
+++ src/pages/Investible/Planning/PlanningInvestibleEdit.js
@@ -13,8 +13,6 @@ import { processTextAndFilesForSave } from '../../../api/files';
 import { getMarketInfo } from '../../../utils/userFunctions';
 import AssignmentList from '../../Dialog/Planning/AssignmentList';
 import SpinBlockingButton from '../../../components/SpinBlocking/SpinBlockingButton';
-import { getMyUserForMarket } from '../../../contexts/MarketsContext/marketsContextHelper';
-import { MarketsContext } from '../../../contexts/MarketsContext/MarketsContext';
 import SpinBlockingButtonGroup from '../../../components/SpinBlocking/SpinBlockingButtonGroup';
 import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext';
 

082b171d7fe042c380851ebfaa05489176c89b17
Ben Follis <bfollis@uclusion.com>
2020-04-02 15:11:40 -0700

    Fix: The service worker now no longer uses the uclusion UI url, which was null in stage

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -23,20 +23,12 @@ const isLocalhost = Boolean(
 export function register(config) {
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.REACT_APP_UI_URL}/image-url-rewriter-service-worker.js`;
+      const swUrl = `/image-url-rewriter-service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-            'worker. To learn more, visit https://bit.ly/CRA-PWA'
-          );
-        });
       } else {
         // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);

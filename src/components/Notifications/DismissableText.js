7a92e041f307006508fc959e980be4d6b48a9c82
Tom Pliner <tompliner@gmail.com>
2020-04-12 12:41:23 -0500

    Style DismissableText component to match figma

--- src/components/Notifications/DismissableText.js
+++ src/components/Notifications/DismissableText.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { DISMISS, DismissTextContext } from '../../contexts/DismissTextContext';
 import IconButton from '@material-ui/core/IconButton';
-import LiveHelpIcon from '@material-ui/icons/LiveHelp';
-import CloseIcon from '@material-ui/icons/Close';
+import LiveHelpTwoToneIcon from '@material-ui/icons/LiveHelpTwoTone';
+import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -15,12 +15,39 @@ const useStyles = makeStyles(theme => ({
     padding: 0,
   },
   leftMost: {
+    margin: 0,
     marginLeft: theme.spacing(1),
-    fontSize: 20,
+    fontSize: 16,
+    flex: 10.5,
+    padding: '14px',
+    display: 'flex'
+  },
+  rightMost: {
+    flex: 1.5,
+    marginLeft: 'auto',
+    margin: 0,
+    display: 'flex',
+    justifyContent: 'flex-end'
   },
   dismissText: {
     transform: 'scale(0.7)',
+    margin: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
   },
+  pointer: {
+    cursor: 'pointer',
+    textDecoration: 'underline'
+  },
+  hoverState: {
+    '&:hover': {
+      backgroundColor: "transparent",
+    }
+  },
+  help: {
+    marginRight: '6px'
+  }
 }));
 
 function DismissableText(props) {
@@ -41,15 +68,17 @@ function DismissableText(props) {
   return (
     <dl className={classes.root} >
       <dl className={classes.leftMost}>
-        <LiveHelpIcon color='primary' />
+        <LiveHelpTwoToneIcon color="inherit" fontSize="small" className={classes.help}/>
         <FormattedMessage id={textId} />
       </dl>
-      <dl>
+      <dl className={classes.rightMost}>
         <dd className={classes.dismissText}>
-          <FormattedMessage id="decisionDialogsDismissDialog" />
-          <IconButton onClick={dismiss}>
-            <CloseIcon />
-          </IconButton>
+          <span onClick={dismiss} className={classes.pointer}>
+            <FormattedMessage id="decisionDialogsDismissDialog" />
+            <IconButton className={classes.hoverState} disableRipple>
+              <CancelRoundedIcon />
+            </IconButton>
+          </span>
         </dd>
       </dl>
     </dl>

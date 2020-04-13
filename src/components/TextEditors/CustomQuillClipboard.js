b336ff6c9f8eaf301ee0a8a67a9d95335db5d221
Ben Follis <bfollis@uclusion.com>
2020-04-13 15:12:38 -0700

    Fix: Hacked up the quill paste module to not supress whitespace

--- src/components/TextEditors/CustomQuillClipboard.js
+++ src/components/TextEditors/CustomQuillClipboard.js
@@ -3,7 +3,7 @@ import isUrl from 'is-url';
 import _ from 'lodash';
 
 const Clipboard = Quill.import('modules/clipboard');
-
+let BlockEmbed = Quill.import('blots/block/embed');
 /**
  * The custom quill clipboard disables pasting in images via direct embedding the URL (we don't support
  * cross site scripts like that), and the quill native file upload
@@ -22,7 +22,139 @@ function stripImageTags(html){
   return filtered;
 }
 
+/// copied from original source code to bring it into scope
+function deltaEndsWith(delta, text) {
+  let endText = '';
+  for (
+    let i = delta.ops.length - 1;
+    i >= 0 && endText.length < text.length;
+    --i // eslint-disable-line no-plusplus
+  ) {
+    const op = delta.ops[i];
+    if (typeof op.insert !== 'string') break;
+    endText = op.insert + endText;
+  }
+  return endText.slice(-1 * text.length) === text;
+}
+function myMatchNewLine(node, delta, scroll) {
+  if (!deltaEndsWith(delta, '\n')) {
+    if (isLine(node)) {
+      return delta.insert('\n');
+    }
+    if (delta.length() > 0 && node.nextSibling) {
+      let { nextSibling } = node;
+      while (nextSibling != null) {
+        if (isLine(nextSibling)) {
+          return delta.insert('\n');
+        }
+        const match = scroll.query(nextSibling);
+        if (match && match.prototype instanceof BlockEmbed) {
+          return delta.insert('\n');
+        }
+        nextSibling = nextSibling.firstChild;
+      }
+    }
+  }
+  return delta;
+}
+function isLine(node) {
+  if (node.childNodes.length === 0) return false; // Exclude embed blocks
+  return [
+    'address',
+    'article',
+    'blockquote',
+    'canvas',
+    'dd',
+    'div',
+    'dl',
+    'dt',
+    'fieldset',
+    'figcaption',
+    'figure',
+    'footer',
+    'form',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'header',
+    'iframe',
+    'li',
+    'main',
+    'nav',
+    'ol',
+    'output',
+    'p',
+    'pre',
+    'section',
+    'table',
+    'td',
+    'tr',
+    'ul',
+    'video',
+  ].includes(node.tagName.toLowerCase());
+}
+const preNodes = new WeakMap();
+function isPre(node) {
+  if (node == null) return false;
+  if (!preNodes.has(node)) {
+    if (node.tagName === 'PRE') {
+      preNodes.set(node, true);
+    } else {
+      preNodes.set(node, isPre(node.parentNode));
+    }
+  }
+  return preNodes.get(node);
+}
+
+function matchTextWithIndents(node, delta) {
+  let text = node.data;
+  // Word represents empty line with <o:p>&nbsp;</o:p>
+  if (node.parentNode.tagName === 'O:P') {
+    return delta.insert(text.trim());
+  }
+  if (text.trim().length === 0 && text.includes('\n')) {
+    return delta;
+  }
+  if (!isPre(node)) {
+    const replacer = (collapse, match) => {
+      const replaced = match.replace(/[^\u00a0]/g, ''); // \u00a0 is nbsp;
+      return replaced.length < 1 && collapse ? ' ' : replaced;
+    };
+    text = text.replace(/\r\n/g, ' ').replace(/\n/g, ' ');
+ //   text = text.replace(/\s\s+/g, replacer.bind(replacer, true)); // collapse whitespace
+    if (
+      (node.previousSibling == null && isLine(node.parentNode)) ||
+      (node.previousSibling != null && isLine(node.previousSibling))
+    ) {
+      text = text.replace(/^\s+/, replacer.bind(replacer, false));
+    }
+    if (
+      (node.nextSibling == null && isLine(node.parentNode)) ||
+      (node.nextSibling != null && isLine(node.nextSibling))
+    ) {
+      text = text.replace(/\s+$/, replacer.bind(replacer, false));
+    }
+
+  }
+
+  return delta.insert(text);
+}
+
 class CustomQuillClipboard extends Clipboard {
+
+  constructor(quill, options) {
+    // override the original matchers to deal better with indents
+    super(quill, options);
+    // remove all the text node matchers so we can add ours back in
+    this.matchers = this.matchers.filter((matcher) => {
+      const [selector,] = matcher;
+      return selector !== Node.TEXT_NODE;
+    });
+    this.matchers = [...this.matchers, [Node.TEXT_NODE, matchTextWithIndents], [Node.TEXT_NODE, myMatchNewLine]];
+  }
 
   onCapturePaste(e) {
     // mostly cribbed from the real implementation
@@ -36,7 +168,7 @@ class CustomQuillClipboard extends Clipboard {
     let text = e.clipboardData.getData('text/plain');
     if (_.isEmpty(filteredHtml)){
       if(isUrl(text)){
-        const encoded = encodeURI(text)
+        const encoded = encodeURI(text);
         filteredHtml = `<a href="${encoded}">${text}</a>`;
         text = undefined;
       }
@@ -44,6 +176,10 @@ class CustomQuillClipboard extends Clipboard {
 
     this.onPaste(range, { html: filteredHtml, text });
   }
+
+
+
+
 }
 
 export default CustomQuillClipboard;

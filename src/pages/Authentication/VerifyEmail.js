591e96a4235f8b9ba05cd5913a7cbf1c913bbe3c
Ben Follis <bfollis@uclusion.com>
2020-04-16 18:53:10 -0700

    Fix: Syntax fixes and verify email now signs you out in order to force you to have the cognito user with an account bound

--- src/pages/Authentication/VerifyEmail.js
+++ src/pages/Authentication/VerifyEmail.js
@@ -1,78 +1,88 @@
-import React, { useEffect, useState } from "react";
-import _ from "lodash";
-import { useHistory } from "react-router";
-import Container from "@material-ui/core/Container";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import { verifyEmail } from "../../api/sso";
-import { redirectToPath, setRedirect } from "../../utils/redirectUtils";
-import { sendIntlMessageBase, ERROR } from "../../utils/userMessage";
-import { useIntl } from 'react-intl'
+import React, { useEffect, useState } from 'react';
+import _ from 'lodash';
+import { useHistory } from 'react-router';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { verifyEmail } from '../../api/sso';
+import { redirectToPath, setRedirect } from '../../utils/redirectUtils';
+import { sendIntlMessageBase, ERROR } from '../../utils/userMessage';
+import { useIntl } from 'react-intl';
+import { Auth } from 'aws-amplify';
 
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: "#3f6b72",
+    backgroundColor: '#3f6b72',
   },
   form: {
-    width: "100%",
+    width: '100%',
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    backgroundColor: "#3f6b72",
-    color: "#fff",
+    backgroundColor: '#3f6b72',
+    color: '#fff',
   },
 }));
 
-function VerifyEmail(props) {
-  const LOGIN = "/";
-  const ALTERNATE_SIDEBAR_LOGO = "Uclusion_Logo_White_Micro.png";
+function VerifyEmail (props) {
+  const LOGIN = '/';
+  const ALTERNATE_SIDEBAR_LOGO = 'Uclusion_Logo_White_Micro.png';
   const history = useHistory();
   const intl = useIntl();
   const classes = useStyles();
   const params = new URL(document.location).searchParams;
   const [verificationState, setVerificationState] = useState(undefined);
-  const code = params.get("code");
+  const code = params.get('code');
+  const { authState } = props;
 
   useEffect(() => {
-    function initiateRedirect() {
+    function initiateRedirect () {
       setTimeout(() => {
         // console.debug("redirecting you to login");
         redirectToPath(history, LOGIN);
       }, 5000);
     }
+
+    function doRedirect (result) {
+      const { redirect } = result;
+      if (!_.isEmpty(redirect)) {
+        // console.log(`Setting redirect to ${redirect}`);
+        setRedirect(redirect);
+      }
+      setVerificationState('VERIFIED');
+      initiateRedirect();
+    }
+
     if (code && !verificationState) {
       verifyEmail(code)
         .then(result => {
+          // we unconditionally sign out in case they are signed in to the user in another tab.
+          // if it fails, we weren't logged in.
+          return Auth.signOut()
+            .then(() => doRedirect(result)) //
+            .catch(() => doRedirect(result));
           // console.log(result);
-          const { redirect } = result;
-          if (!_.isEmpty(redirect)) {
-            // console.log(`Setting redirect to ${redirect}`);
-            setRedirect(redirect);
-          }
-          setVerificationState("VERIFIED");
-          initiateRedirect();
-        })
-        .catch((error) => {
-          console.error(error);
-          sendIntlMessageBase(intl, ERROR, "errorVerifyFailed");
-        });
+        }).catch((error) => {
+        console.error(error);
+        sendIntlMessageBase(intl, ERROR, 'errorVerifyFailed');
+      });
     }
-  }, [code, verificationState, history, intl]);
+  }, [code, verificationState, history, intl, authState]);
 
   if (!code) {
     return (
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
+        <CssBaseline/>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <img
@@ -91,10 +101,10 @@ function VerifyEmail(props) {
     );
   }
 
-  if (verificationState === "ALREADY_EXISTS") {
+  if (verificationState === 'ALREADY_EXISTS') {
     return (
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
+        <CssBaseline/>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <img
@@ -113,10 +123,10 @@ function VerifyEmail(props) {
     );
   }
 
-  if (verificationState === "VERIFIED") {
+  if (verificationState === 'VERIFIED') {
     return (
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
+        <CssBaseline/>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <img
@@ -137,7 +147,7 @@ function VerifyEmail(props) {
 
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
+      <CssBaseline/>
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <img

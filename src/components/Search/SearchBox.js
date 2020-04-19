33b49395f206ff7dacc165de9a31a407d438f88a
disrael <disrael@gmail.com>
2020-04-18 23:04:13 -0700

    fix: de-dupe search comment results

--- src/components/Search/SearchBox.js
+++ src/components/Search/SearchBox.js
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react';
-import { SearchIndexContext } from '../../contexts/SearchIndexContext/SearchIndexContext';
-import { TextField, InputAdornment } from '@material-ui/core';
-import _ from 'lodash';
-import SearchIcon from '@material-ui/icons/Search';
-import { useIntl } from 'react-intl';
-import { SearchResultsContext } from '../../contexts/SearchResultsContext/SearchResultsContext';
+import React, { useContext } from 'react'
+import { SearchIndexContext } from '../../contexts/SearchIndexContext/SearchIndexContext'
+import { InputAdornment, TextField } from '@material-ui/core'
+import _ from 'lodash'
+import SearchIcon from '@material-ui/icons/Search'
+import { useIntl } from 'react-intl'
+import { SearchResultsContext } from '../../contexts/SearchResultsContext/SearchResultsContext'
+import { INDEX_COMMENT_TYPE } from '../../contexts/SearchIndexContext/searchIndexContextMessages'
+import { getCommentRoot } from '../../contexts/CommentsContext/commentsContextHelper'
+import { CommentsContext } from '../../contexts/CommentsContext/CommentsContext'
 
 function SearchBox (props) {
   const MAX_RESULTS = 15;
   const intl = useIntl();
   const [index] = useContext(SearchIndexContext);
   const [searchResults, setSearchResults] = useContext(SearchResultsContext);
-
-
+  const [commentsState] = useContext(CommentsContext);
+  function isEqual(a, b) {
+    if (a.type !== b.type) {
+      return false;
+    }
+    if (a.type === INDEX_COMMENT_TYPE) {
+      const commentRootA = getCommentRoot(commentsState, a.marketId, a.id);
+      const commentRootB = getCommentRoot(commentsState, b.marketId, b.id);
+      return _.isEqual(commentRootA, commentRootB);
+    }
+    return a.id === b.id && a.marketId === b.marketId;
+  }
   function onSearchChange (event) {
     const { value } = event.target;
-    // return the first 15 results
-    const results = _.take(index.search(value), MAX_RESULTS);
+    const results = _.uniqWith(_.take(index.search(value), MAX_RESULTS), isEqual);
     setSearchResults({
       search: value,
       results

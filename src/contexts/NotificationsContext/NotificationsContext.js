2abe763fdfd7d78a3a2ee07ff817921d31d50b16
Ben Follis <bfollis@uclusion.com>
2020-04-20 18:07:05 -0700

    Fix: Rework of notifiy to make it more readable

--- src/contexts/NotificationsContext/NotificationsContext.js
+++ src/contexts/NotificationsContext/NotificationsContext.js
@@ -20,6 +20,7 @@ import { navigate } from '../../utils/marketIdPathFunctions';
 import { getFullLink } from '../../components/Notifications/Notifications';
 import { messageComparator } from '../../utils/messageUtils';
 
+
 export const EMPTY_STATE = {
   initializing: true,
   messages: [],
@@ -30,7 +31,6 @@ const NotificationsContext = React.createContext(EMPTY_STATE);
 export const VISIT_CHANNEL = 'VisitChannel';
 export const VIEW_EVENT = 'pageView';
 
-
 function NotificationsProvider (props) {
   // eslint-disable-next-line react/prop-types
   const { children } = props;
@@ -38,8 +38,7 @@ function NotificationsProvider (props) {
   const { page, messages, lastPage } = state;
   const [diffState] = useContext(DiffContext);
   const [isInitialization, setIsInitialization] = useState(true);
-  const [isProcessingPage, setIsProcessingPage] = useState(undefined);
-  const [isProcessingPageInitial, setIsProcessingPageInitial] = useState(true);
+  const [currentlyProcessingPage, setCurrentlyProcessingPage] = useState(undefined);
   const [, highlightedCommentDispatch] = useContext(HighlightedCommentContext);
   const [, highlightedVotingDispatch] = useContext(HighlightedVotingContext);
   const history = useHistory();
@@ -63,7 +62,10 @@ function NotificationsProvider (props) {
     };
   }, [isInitialization]);
 
+
+
   useLayoutEffect(() => {
+    // First some useful helper functions
     function getMessagesForPage () {
       return messages.filter((message) => {
         const { marketId, investibleId, action, beingProcessed } = page;
@@ -90,125 +92,144 @@ function NotificationsProvider (props) {
       });
     }
 
-    if (page && !pageIsEqual(page, isProcessingPage)) {
-      if (isProcessingPageInitial) {
-        setIsProcessingPage(page);
-        // Too confusing for the user if multiple processing happening at once so ignore everything for 5s
-        setTimeout(() => {
-          setIsProcessingPageInitial(false);
-          setIsProcessingPage(undefined);
-        }, 5000);
+    function newUserMessageProcessed() {
+      const newUserMessage = messages.find((massagedMessage) => massagedMessage.pokeType === 'new_user');
+      if (newUserMessage) {
+        if (pathname === '/') {
+          // This is a new user going to home page but we need to redirect
+          navigate(history, '/dialogAdd#type=PLANNING');
+          return true;
+        } else {
+          const { marketId, action } = page;
+          let removeNewUserNotification = false;
+          if (marketId) {
+            // This user is coming in on a market invite
+            removeNewUserNotification = true;
+          } else if (action === 'dialogAdd') {
+            // This new user reached the dialog add page successfully so delete this message
+            // Otherwise they might click on the notification in the tray when already where supposed to be
+            removeNewUserNotification = true;
+          }
+          if (removeNewUserNotification) {
+            dispatch(processedPage(page, [newUserMessage], {}));
+            return true;
+          }
+        }
       }
-      let isOldPage = lastPage !== undefined && pageIsEqual(page, lastPage);
-      // If last page and page are equal then we last processed this page and consider the user as already reading it
-      page.lastProcessed = Date.now();
-      const scrollTarget = (hash && hash.length > 1) ? hash.substring(1, hash.length) : undefined;
-      if (!isOldPage && !hash) {
-        //Scroll to the top if its a new page and there is no anchor to scroll to
-        window.scrollTo(0, 0);
-      }
-      if (_.isEmpty(messages)) {
-        if (!isOldPage) {
-          // If there are no new messages on a new page then just scroll and mark old
-          dispatch(processedPage(page, undefined, undefined, true, scrollTarget));
-        }
-      } else {
-        const newUserMessage = messages.find((massagedMessage) => massagedMessage.pokeType === 'new_user');
-        if (newUserMessage) {
-          if (pathname === '/') {
-            // This is a new user going to home page but we need to redirect
-            navigate(history, '/dialogAdd#type=PLANNING');
-          } else {
-            const { marketId, action } = page;
-            let removeNewUserNotification = false;
-            if (marketId) {
-              // This user is coming in on a market invite
-              removeNewUserNotification = true;
-            } else if (action === 'dialogAdd') {
-              // This new user reached the dialog add page successfully so delete this message
-              // Otherwise they might click on the notification in the tray when already where supposed to be
-              removeNewUserNotification = true;
-            }
-            if (removeNewUserNotification) {
-              dispatch(processedPage(page, [newUserMessage], {}));
-              return;
-            }
-          }
-        }
-        const pageMessages = getMessagesForPage();
-        if (_.isEmpty(pageMessages)) {
-          // if we have any messages for this page, and we're not an old page
-          // tell the store that we've processed them
-          if (!isOldPage) {
-            dispatch(processedPage(page, undefined, undefined, true, scrollTarget));
-          }
-          return;
-        }
-        if (!isProcessingPageInitial) {
-          setIsProcessingPage(page);
-          // More came in on this page so put the guard back up
-          setTimeout(() => {
-            setIsProcessingPageInitial(true);
-            setIsProcessingPage(undefined);
-          }, 5000);
-        }
-        //If you've been on the page less than 3s count as new for the purposes of new messages
-        isOldPage = isOldPage && (Date.now() - page.lastProcessed > 3000);
-        // Process the messages that match this page
-        pageMessages.forEach((message) => {
-          const {
-            level,
-            commentId,
-            associatedUserId,
-          } = message;
-          if (commentId) {
-            highlightedCommentDispatch({ type: HIGHTLIGHT_ADD, commentId, level });
-          }
-          if (associatedUserId) {
-            highlightedVotingDispatch({ type: HIGHTLIGHT_ADD, associatedUserId, level });
-          }
-        });
-        // sort the messages so we processes them in the right order
-        pageMessages.sort(messageComparator);
-        // We only link to one message and we only need one message to tell the delete API what page we are on
-        // so it can delete all messages associated with that page.
-        const message = pageMessages[0];
-        let toastInfo = {};
-        const {
-          marketId,
-          investibleId,
-          text,
-          level,
-          aType,
-          commentId,
-        } = message;
-        // If there are multiple new RED on this page not much we can do - not a bug tracker
-        const multiUpdate = pageMessages.length > 1 && level !== 'RED';
-        // Sadly intl not available here TODO - Fix
-        const myText = multiUpdate ? `${pageMessages.length} Updates` : text;
-        const diffId = commentId || investibleId || marketId;
-        const linkNotMatching = getFullLink(message) !== `${pathname}${hash}`;
-        // Do not toast a non red unread as already have diff and dismiss - unless is new
-        // Do toast if the page hasn't changed since will not scroll in that case and need toast if want to scroll
-        const shouldToast = (multiUpdate || isOldPage || (!isOldPage && linkNotMatching))
-          || (level === 'RED') || (!commentId && (aType !== 'UNREAD' || hasUnViewedDiff(diffState, diffId)));
-        const myCustomToastId = myText + '_' + diffId;
-        if (shouldToast && !toast.isActive(myCustomToastId)) {
-          //Tell the notifications reducer what to do but only do it inside the dispatch for fear of processing twice
-          const options = {
-            onClick: () => navigate(history, getFullLink(message)),
-            toastId: myCustomToastId
-          };
-          toastInfo = { myText, level, options };
-        }
-        dispatch(processedPage(page, pageMessages, toastInfo, !isOldPage, scrollTarget));
-      }
+      return false;
     }
+
+    function processMessageHighlights(pageMessages) {
+      // Set up the highlights based on the page messages
+      pageMessages.forEach((message) => {
+        const {
+          level,
+          commentId,
+          associatedUserId,
+        } = message;
+        if (commentId) {
+          highlightedCommentDispatch({ type: HIGHTLIGHT_ADD, commentId, level });
+        }
+        if (associatedUserId) {
+          highlightedVotingDispatch({ type: HIGHTLIGHT_ADD, associatedUserId, level });
+        }
+      });
+    }
+
+    if (_.isEmpty(page)) {
+      // if we don't have any page info then we can't do anything
+      return;
+    }
+    const onSamePageAsLastProcess = lastPage !== undefined && pageIsEqual(page, lastPage);
+    // If last page and page are equal then we last processed this page and consider the user as already reading it
+    // we'll need to update the process time for the page to the current time
+    page.lastProcessed = Date.now();
+    const scrollTarget = (hash && hash.length > 1) ? hash.substring(1, hash.length) : undefined;
+    if (!onSamePageAsLastProcess && !scrollTarget) {
+      //Scroll to the top if its a new page and there is no anchor to scroll to
+      window.scrollTo(0, 0);
+    }
+    if (_.isEmpty(messages)) {
+      if (!onSamePageAsLastProcess) {
+        // If there are no new messages on a new page then just scroll and mark old
+        dispatch(processedPage(page, undefined, undefined, true, scrollTarget));
+      }
+      // if we've no messages, then we have nothing new to do
+      return;
+    }
+    // if the message we just processed was a new user message, then we
+    // need to process that and stop processing for this run
+    const wasNewUserMessage = newUserMessageProcessed();
+    if (wasNewUserMessage) {
+      return;
+    }
+
+    const pageMessages = getMessagesForPage();
+    if (_.isEmpty(pageMessages)) {
+      // if we don't have any messages for this page, and we've not already processed this page
+      // tell the store that we've landed here
+      if (!onSamePageAsLastProcess) {
+        dispatch(processedPage(page, undefined, undefined, true, scrollTarget));
+      }
+      return;
+    }
+    // let the context know we're working on this page
+    setCurrentlyProcessingPage(page);
+    //If you've been on the page less than 3s count page process new for the purposes of new messages in this page
+    const pageChangedRecently = !onSamePageAsLastProcess || (onSamePageAsLastProcess && (Date.now() - page.lastProcessed > 3000));
+    // do all the highlighting
+    processMessageHighlights(pageMessages);
+    // sort the messages so we toast them in the right order
+    pageMessages.sort(messageComparator);
+    // We only allow single messages to be linked
+    // and we only need one message to tell the delete API what page we are on
+    // so it can delete all messages associated with that page.
+    // Therefore we'll use the first message as representative
+    const message = pageMessages[0];
+    const messageLink = getFullLink(message);
+    let toastInfo = {};
+    const {
+      marketId,
+      investibleId,
+      text,
+      level,
+      aType,
+      commentId,
+    } = message;
+    // due to the sort above we're guaranteed that if the message is NOT red, than neither are any other
+    // messages in the page messages
+    const messageIsRed = level === 'RED';
+    // If there are multiple new RED on this page not much we can do - not a bug tracker
+    const shouldCollapseMessages = !messageIsRed && pageMessages.length > 1;
+    // Sadly intl not available here TODO - Fix
+    const myText = shouldCollapseMessages ? `${pageMessages.length} Updates` : text;
+    const diffId = investibleId || marketId; // we don't diff comments
+    // get the object ID this message pertains to
+    const objectId = commentId || investibleId || marketId;
+    const messageIsForDifferentPage = messageLink !== `${pathname}${hash}`;
+    // there will be no diff if this message is for a comment, otherwise, go looking for an unviewed
+    // as we do not toast a non red unread as already have diff and dismiss - unless is new
+    const diffAvailable = (!commentId && (aType !== 'UNREAD' || hasUnViewedDiff(diffState, diffId)));
+    // Do toast if the page hasn't changed since will not scroll in that case and need toast if want to scroll
+    const shouldToast = (shouldCollapseMessages || pageChangedRecently || (!pageChangedRecently && messageIsForDifferentPage))
+      || messageIsRed || diffAvailable;
+    const myCustomToastId = myText + '_' + objectId;
+    // if we have a toast up already for this object and text, do nothing
+    if (shouldToast && !toast.isActive(myCustomToastId)) {
+      //Tell the notifications reducer what to do but only do it inside the dispatch for fear of processing twice
+      const options = {
+        onClick: () => navigate(history, messageLink),
+        toastId: myCustomToastId
+      };
+      toastInfo = { myText, level, options };
+    }
+    dispatch(processedPage(page, pageMessages, toastInfo, !pageChangedRecently, scrollTarget));
+
     return () => {
     };
   }, [
     page, messages, diffState, history, lastPage, highlightedCommentDispatch, highlightedVotingDispatch,
-    pathname, hash, isProcessingPage, isProcessingPageInitial
+    pathname, hash, currentlyProcessingPage
   ]);
 
   return (

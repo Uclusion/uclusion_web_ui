dc3bb8da847e7f29f5312785a1fee8724cac0fd0
disrael <disrael@gmail.com>
2020-04-24 12:58:08 -0700

    fix: do not auto navigate back to market after move next stage

--- src/pages/Investible/Planning/MoveToNextVisibleStageActionButton.js
+++ src/pages/Investible/Planning/MoveToNextVisibleStageActionButton.js
@@ -3,13 +3,10 @@ import PropTypes from 'prop-types'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
 import { MarketStagesContext } from '../../../contexts/MarketStagesContext/MarketStagesContext'
 import { getAcceptedStage, getInReviewStage, } from '../../../contexts/MarketStagesContext/marketStagesContextHelper'
-import { formMarketLink, navigate } from '../../../utils/marketIdPathFunctions'
-import { useHistory } from 'react-router'
 import StageChangeAction from '../../../components/SidebarActions/Planning/StageChangeAction'
 
 function MoveToNextVisibleStageActionButton(props) {
   const { marketId, currentStageId, disabled } = props;
-  const history = useHistory();
   const [marketStagesState] = useContext(MarketStagesContext);
   const acceptedStage = getAcceptedStage(marketStagesState, marketId) || {};
   let destinationStage = acceptedStage;
@@ -33,7 +30,6 @@ function MoveToNextVisibleStageActionButton(props) {
       explanationId={destinationExplanation}
       targetStageId={destinationStage.id}
       disabled={disabled}
-      onSpinStop={() => navigate(history, formMarketLink(marketId))}
     />
   );
 }

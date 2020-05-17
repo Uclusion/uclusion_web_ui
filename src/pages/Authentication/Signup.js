7baea51a2a68051f57e961b0ac45b99bfb8ca693
disrael <disrael@gmail.com>
2020-05-16 18:48:18 -0700

    fix: github login didn't redirect

--- src/pages/Authentication/Signup.js
+++ src/pages/Authentication/Signup.js
@@ -323,13 +323,16 @@ function Signup(props) {
               paddingRight: '0px'
             }}
             align="center"
-            onClick={() => Auth.federatedSignIn({provider: 'GithubLogin'})}>
+            onClick={() => {
+              setRedirect(getRedirect());
+              Auth.federatedSignIn({provider: 'GithubLogin'});
+            }}>
               Sign up with Github
           </GithubLoginButton>
           <div className={classes.googleButton} onClick={() => {
             // Must come back to this device so go ahead and set in local storage
             setRedirect(getRedirect());
-            Auth.federatedSignIn({provider: 'Google'})
+            Auth.federatedSignIn({provider: 'Google'});
           }}>
             <img className={classes.googleImg} alt="Sign in with Google" src={`/images/btn_google_dark_normal_ios.svg`} />
             <div className={classes.googleText}>Sign up with Google</div>

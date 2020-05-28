03e2ee057ba2bd6e2812ae958d322329f62d38d9
disrael <disrael@gmail.com>
2020-05-27 17:00:35 -0700

    fix: verbiage and minor UI adjustments

--- src/pages/Onboarding/SignupOnboarding/Workspace/StoryWorkspace/CurrentStoryProgressStep.js
+++ src/pages/Onboarding/SignupOnboarding/Workspace/StoryWorkspace/CurrentStoryProgressStep.js
@@ -6,7 +6,7 @@ import _ from 'lodash'
 import StepButtons from '../../../StepButtons'
 import QuillEditor from '../../../../../components/TextEditors/QuillEditor'
 import { DaysEstimate } from '../../../../../components/AgilePlan'
-import { updateValues } from '../../../onboardingReducer';
+import { updateValues } from '../../../onboardingReducer'
 
 function CurrentStoryProgressStep (props) {
   const { updateFormData, formData, active, classes } = props;
@@ -15,7 +15,7 @@ function CurrentStoryProgressStep (props) {
     currentStoryProgress,
     currentStoryEstimate,
   } = formData;
-  console.log(formData);
+
   const [editorContents, setEditorContents] = useState(currentStoryProgress || '');
 
   const validForm = !_.isEmpty(editorContents) && _.isNumber(currentStoryEstimate);
@@ -53,9 +53,9 @@ function CurrentStoryProgressStep (props) {
   return (
     <div>
       <Typography variant="body1">
-        Great, now that we have what you're currently working on, we can provide everyone a status update by
-        telling everyone when you expect to be done and what they current status is. If you don't know these things,
-        right now, we'll remind you to fill it in later, so you can simply hit 'Skip' for now.
+        Great, now that we have what you're currently working on, we can provide an update by
+        telling everyone when you expect to be done and the current status. Fill in as much as you know or
+        simply hit 'Skip'.
       </Typography>
       <div className={classes.spacer}></div>
       <QuillEditor
@@ -65,8 +65,9 @@ function CurrentStoryProgressStep (props) {
         simple
         onChange={onEditorChange}
       />
+      <div className={classes.borderBottom}></div>
       <div className={classes.dateContainer}>
-      <label className={classes.inputLabel}>{intl.formatMessage({ id: "daysEstimateMarketLabel" })}</label>
+        <label className={classes.inputLabel}>{intl.formatMessage({ id: "daysEstimateMarketLabel" })}</label>
         <DaysEstimate showLabel={false} showHelper={false} onChange={onEstimateChange} value={currentStoryEstimate} createdAt={new Date()} />
       </div>
       <div className={classes.borderBottom}></div>

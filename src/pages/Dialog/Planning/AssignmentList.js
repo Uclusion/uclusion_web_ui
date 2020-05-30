17105a551bf862ff276e14adf06414e1f2c9ba79
Tom Pliner <tompliner@gmail.com>
2020-05-30 16:04:45 -0500

    filterable assignment list for stories like collaborators

--- src/pages/Dialog/Planning/AssignmentList.js
+++ src/pages/Dialog/Planning/AssignmentList.js
@@ -1,18 +1,26 @@
 import React, { useContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
-import { Checkbox, List, ListItem, ListItemIcon, ListItemText, ListSubheader, makeStyles, } from '@material-ui/core'
+import clsx from 'clsx';
+import { Checkbox, List, ListItem, ListItemIcon, ListItemText, ListSubheader, makeStyles, TextField, InputAdornment, IconButton } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 import { useIntl } from 'react-intl'
 import { getMarketPresences } from '../../../contexts/MarketPresencesContext/marketPresencesHelper'
 import { MarketPresencesContext } from '../../../contexts/MarketPresencesContext/MarketPresencesContext'
 import { getMyUserForMarket } from '../../../contexts/MarketsContext/marketsContextHelper'
 import { MarketsContext } from '../../../contexts/MarketsContext/MarketsContext'
+import { usePlanFormStyles } from '../../../components/AgilePlan'
 
 const useStyles = makeStyles((theme) => ({
-  name: {},
+  name: {
+    width: '50%',
+  },
   disabled: {
     color: theme.palette.text.disabled,
   },
+  scrollContainerHeight: {
+    height: '230px'
+  }
 }));
 
 function AssignmentList(props) {
@@ -30,6 +38,7 @@ function AssignmentList(props) {
   const marketPresences = fullMarketPresences.filter((presence) => !presence.market_banned);
   const [marketsState] = useContext(MarketsContext);
   const userId = getMyUserForMarket(marketsState, marketId) || {};
+  const formClasses = usePlanFormStyles();
 
   function getDefaultChecked() {
     if (!_.isEmpty(previouslyAssigned)) {
@@ -41,7 +50,11 @@ function AssignmentList(props) {
     return { [userId]: true };
   }
 
+  const participantEntries = getSortedPresenceWithAssignable();
   const [checked, setChecked] = useState(getDefaultChecked());
+  const [searchValue, setSearchValue] = useState(undefined);
+  const [participants, setParticipants] = useState(participantEntries)
+  const [filteredNames, setFilteredNames] = useState(undefined);
   const [submitted, setSubmitted] = useState(getDefaultChecked());
 
   useEffect(() => {
@@ -75,7 +88,10 @@ function AssignmentList(props) {
     return () => {
     };
   }
-
+  function onSearchChange (event) {
+    const { value } = event.target;
+    setSearchValue(value);
+  }
   function renderParticipantEntry(presenceEntry) {
     const { name, assignable, id } = presenceEntry;
     const alreadyAssigned = previouslyAssigned.includes(id);
@@ -85,6 +101,7 @@ function AssignmentList(props) {
       <ListItem
         key={id}
         button
+        className={ boxChecked ? clsx( formClasses.unselected, formClasses.selected ) : formClasses.unselected }
         onClick={getCheckToggle(canBeAssigned, id)}
       >
         <ListItemIcon>
@@ -103,16 +120,67 @@ function AssignmentList(props) {
     );
   }
 
-  const participantEntries = getSortedPresenceWithAssignable();
+
+  useEffect(() => {
+    if (!searchValue) {
+      setFilteredNames(undefined);
+    } else if (participants) {
+      const searchValueLower = searchValue.toLowerCase();
+      const filteredEntries = participants.filter((entry) => {
+        const { name } = entry;
+        const nameLower = name.toLowerCase();
+        let index = 0;
+        // eslint-disable-next-line no-restricted-syntax
+        for (const c of searchValueLower) {
+          const foundIndex = _.indexOf(nameLower, c, index);
+          if (foundIndex < 0) {
+            return false;
+          }
+          index = foundIndex;
+        }
+        return true;
+      });
+      if(filteredNames === undefined || filteredEntries.length !== filteredNames.length){
+        setFilteredNames(filteredEntries);
+      }
+    }
+  }, [searchValue, participants, filteredNames]);
+
+  const displayNames = filteredNames || participants || [];
 
   return (
     <List
-      dense
-    >
+    dense
+    className={clsx(formClasses.scrollableList, formClasses.sharedForm, formClasses.paddingRight)}
+  >
+     <ListItem className={formClasses.searchContainer} key="search">
+          <ListItemText >
+            <TextField
+              className={formClasses.search}
+              placeholder="Search in your organization"
+              onChange={onSearchChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position={'end'}>
+                    <IconButton>
+                      <SearchIcon/>
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </ListItemText>
+        </ListItem>
       <ListSubheader>
         {intl.formatMessage({ id: 'assignmentListHeader' })}
       </ListSubheader>
-      {participantEntries.map((entry) => renderParticipantEntry(entry))}
+      <List
+          dense
+          id="addressBook"
+          className={clsx(formClasses.scrollContainer, classes.scrollContainerHeight)}
+        >
+      {displayNames.map((entry) => renderParticipantEntry(entry))}
+      </List>
     </List>
   );
 }

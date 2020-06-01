8202631b89526270c16c0efc20ce3b7ea6c31969
Ben Follis <bfollis@uclusion.com>
2020-06-01 15:52:54 -0700

    Fix: We no longer present the menu item to change your password to users who log in via oauth

---
+++ src/contexts/CognitoUserContext/cognitoUserContextHelper.js
@@ -0,0 +1,14 @@
+/**
+ * Returns whether or not the user is a federated user. That is it comes from
+ * google, github, etc. We can't change attributes on those users. If the user
+ * is empty, returns false.
+ * @param user
+ */
+export function isFederated(user) {
+  console.error(user);
+  // external users have an identities value in their user attributes
+  if (!user) {
+    return false;
+  }
+  return !!user.identities;
+}

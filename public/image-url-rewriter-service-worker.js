9cd5355e34462bb4426c269df5a86eed1b0e6d36
Ben Follis <bfollis@uclusion.com>
2020-06-05 17:01:58 -0700

    Fix: Forced a bunch of updates to make sure serviceworker is newest and loads immediately

--- public/image-url-rewriter-service-worker.js
+++ public/image-url-rewriter-service-worker.js
@@ -1,19 +1,31 @@
 const OUR_FILE_PATTERN = /https\:\/\/\w+.cloudfront.net\/(\w{8}(-\w{4}){3}-\w{12})\/\w{8}(-\w{4}){3}-\w{12}.*/i;
 self.importScripts('localforage.min.js');
+self.addEventListener('install', (event) => {
+  return self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+});
+//console.log('Imported localforage');
 self.addEventListener('fetch', (event) => {
   const { request } = event;
   const { method, url } = request;
+  console.log('Service worker firing');
   if (method === 'GET') {
+   // console.log(`Service worker looking for get for url ${url}`);
     const match = url.match(OUR_FILE_PATTERN);
     if (match) {
       const pathId = match[1]; // it's the first capturing group
       const marketKey = `MARKET_${pathId}`;
       const homeAccountKey = 'ACCOUNT_home_account';
+      //console.log(`Service worker looking for token ${marketKey}`);
       const promise = self.localforage.createInstance({ storeName: 'TOKEN_STORAGE_MANAGER' }).getItem(marketKey)
         .then((token) => {
           if (token) {
+            //console.log(`Service worker found market token ${token}`);
             return token;
           } else {
+            //console.log(`Service worker did not find token for ${marketKey} trying home account`);
             return self.localforage.createInstance({ storeName: 'TOKEN_STORAGE_MANAGER' }).getItem(homeAccountKey);
           }
         }).then((token) => {
@@ -29,3 +41,4 @@ self.addEventListener('fetch', (event) => {
     }
   }
 });
+//console.log('Listener added');

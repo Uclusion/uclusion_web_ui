202beb53ee484e4c9659778aa249f4066c9623d1
disrael <disrael@gmail.com>
2020-06-08 19:15:27 -0700

    fix: Do not allow click out while market is loading from invite

--- src/pages/Invites/MarketInvite.js
+++ src/pages/Invites/MarketInvite.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
 import { useIntl } from 'react-intl'
 import PropTypes from 'prop-types'
 import { useHistory } from 'react-router'
 import _ from 'lodash'
+import Header from '../../containers/Header'
 import { decomposeMarketPath, formMarketLink, navigate, } from '../../utils/marketIdPathFunctions'
-import Screen from '../../containers/Screen/Screen'
 import { VERSIONS_HUB_CHANNEL } from '../../contexts/WebSocketContext'
 import { getMarketFromInvite } from '../../api/uclusionClient'
 import { registerListener } from '../../utils/MessageBusUtils'
@@ -13,6 +14,54 @@ import queryString from 'query-string'
 import { NEW_MARKET } from '../../contexts/VersionsContext/versionsContextMessages'
 import { MarketsContext } from '../../contexts/MarketsContext/MarketsContext'
 import { AccountUserContext } from '../../contexts/AccountUserContext/AccountUserContext'
+import { CircularProgress, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  hidden: {
+    display: 'none',
+  },
+  normal: {},
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  container: {
+    background: '#efefef',
+    padding: '46px 20px 156px',
+  },
+  containerAll: {
+    background: '#efefef',
+    padding: '24px 20px 156px',
+    marginTop: '80px',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      padding: '24px 12px 156px',
+    },
+  },
+  actionContainer: {
+    marginBottom: '-6rem'
+  },
+  content: {
+    background: '#efefef',
+  },
+  elevated: {
+    zIndex: 99,
+  },
+  loadingDisplay: {
+    padding: '95px 20px 156px',
+    width: '100%'
+  },
+  loadingContainer: {
+    justifyContent: 'center',
+    display: 'flex',
+    overflow: 'hidden',
+    marginTop: 'calc(50vh - 60px)'
+  },
+  loadingColor: {
+    fill: '#3f6b72'
+  }
+}));
 
 function MarketInvite(props) {
   const { hidden } = props;
@@ -25,6 +74,7 @@ function MarketInvite(props) {
   const [myLoading, setMyLoading] = useState(undefined);
   const [marketState] = useContext(MarketsContext);
   const [userState] = useContext(AccountUserContext) || {};
+  const classes = useStyles();
 
   useEffect(() => {
     if (!hidden && myLoading !== marketToken && !_.isEmpty(userState)) {
@@ -65,14 +115,29 @@ function MarketInvite(props) {
   }, [hidden, marketToken, history, hash, marketState, myLoading, userState]);
 
   return (
-    <Screen
-      title={intl.formatMessage({ id: 'loadingMarket' })}
-      tabTitle={intl.formatMessage({ id: 'loadingMarket' })}
-      hidden={hidden}
-      loading={true}
+  <div className={hidden ? classes.hidden : classes.normal}>
+    <Helmet
+      defer={false}
     >
-      <div />
-    </Screen>
+      <title>{intl.formatMessage({ id: 'loadingMarket' })}</title>
+    </Helmet>)
+    <Header
+      title={intl.formatMessage({ id: 'loadingMarket' })}
+      breadCrumbs={[]}
+      toolbarButtons={[]}
+      hidden={hidden}
+      appEnabled
+      logoLinkDisabled
+      hideTools
+    />
+    <div className={classes.content}>
+      <Grid container>
+        <Grid item xs={12} className={classes.loadingContainer}>
+          <CircularProgress className={classes.loadingColor} size={120} type="indeterminate"/>
+        </Grid>
+      </Grid>
+    </div>
+  </div>
   );
 }
 

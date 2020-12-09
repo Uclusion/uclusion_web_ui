b5a30ab6701ceeb87ccbaaa5061cf0f7511ee901
disrael <disrael@gmail.com>
2020-12-09 09:24:42 -0800

    feat: default the name from description

---
+++ src/components/TextFields/NameField.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { TextField } from '@material-ui/core';
+import { useIntl } from 'react-intl';
+import _ from 'lodash'
+
+function NameField(props) {
+  const intl = useIntl();
+  const {
+    onEditorChange, onStorageChange, description, name
+  } = props;
+  const debouncedOnStoreChange = _.debounce((value) => {
+    onStorageChange(value);
+  }, 500);
+
+  function findFirst(list) {
+    let found = -1;
+    for (let i = 0, len = list.length; i < len; i++) {
+      let index = description.indexOf(list[i]);
+      if (index >= 0) {
+        if (found < 0 || index < found) found = index;
+      }
+    }
+    if (found >= 0) {
+      return description.substring(0, found);
+    }
+    return undefined;
+  }
+
+  function createDefaultName() {
+    if (description && !name) {
+      const found = findFirst(["</p", "</li", "</td"]);
+      if (found) {
+        onEditorChange(found.replace(/(<([^>]+)>)/ig,''));
+      }
+    }
+  }
+
+  function handleChange(event) {
+    const { value } = event.target;
+    onEditorChange(value);
+    debouncedOnStoreChange(value);
+  }
+
+  return (
+    <TextField
+      onFocus={createDefaultName}
+      fullWidth
+      id="plan-investible-name"
+      label={intl.formatMessage({ id: "agilePlanFormTitleLabel" })}
+      onChange={handleChange}
+      placeholder={intl.formatMessage({
+        id: "storyTitlePlaceholder"
+      })}
+      value={name}
+      variant="filled"
+    />
+  )
+}
+
+NameField.propTypes = {
+  onEditorChange: PropTypes.func.isRequired,
+  onStorageChange: PropTypes.func.isRequired,
+  description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
+
+export default NameField;

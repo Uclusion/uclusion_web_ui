a7fca2a83217d457190267277e5ec695cbd4f69f
Ben Follis <bfollis@uclusion.com>
2021-01-12 17:51:06 -0800

    Fix: excluded red level messages from dismiss

--- src/components/Notifications/NotificationMessageDisplay.js
+++ src/components/Notifications/NotificationMessageDisplay.js
@@ -24,7 +24,7 @@ function NotificationMessageDisplay (props) {
     type
   } = message;
   console.error(message);
-  const dismissable = type === UNREAD_TYPE;
+  const dismissable = type === UNREAD_TYPE && level !== RED_LEVEL;
   const history = useHistory();
   const color = level === RED_LEVEL ? '#ff9b9b' : level === YELLOW_LEVEL ? '#e8e9a9' : '#85bddb';
   const fontColor = level === RED_LEVEL ? 'black' : level === YELLOW_LEVEL ? 'black' : 'white';

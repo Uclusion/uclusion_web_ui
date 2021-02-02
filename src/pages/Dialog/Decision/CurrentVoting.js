c17ded27d3330bc8830b0eb2cbbb542e54b2be40
disrael <disrael@gmail.com>
2021-02-01 16:09:51 -0800

    fix: drag and drop decision investibles

--- src/pages/Dialog/Decision/CurrentVoting.js
+++ src/pages/Dialog/Decision/CurrentVoting.js
@@ -13,6 +13,16 @@ import VoteCard from '../../../components/Cards/VoteCard'
 import useFitText from 'use-fit-text'
 import { findMessageOfTypeAndId } from '../../../utils/messageUtils'
 import { NotificationsContext } from '../../../contexts/NotificationsContext/NotificationsContext'
+import { moveInvestibleToCurrentVoting } from '../../../api/investibles'
+import { refreshInvestibles } from '../../../contexts/InvestibesContext/investiblesContextHelper'
+import { DiffContext } from '../../../contexts/DiffContext/DiffContext'
+import { InvestiblesContext } from '../../../contexts/InvestibesContext/InvestiblesContext'
+import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext'
+import {
+  getInCurrentVotingStage,
+  getProposedOptionsStage
+} from '../../../contexts/MarketStagesContext/marketStagesContextHelper'
+import { MarketStagesContext } from '../../../contexts/MarketStagesContext/MarketStagesContext'
 
 const useStyles = makeStyles(theme => ({
   noPadding: {
@@ -57,6 +67,12 @@ function CurrentVoting(props) {
   const { marketPresences, investibles, marketId, comments, inArchives } = props;
   const strippedInvestibles = investibles.map(inv => inv.investible);
   const [messagesState] = useContext(NotificationsContext);
+  const [marketStagesState] = useContext(MarketStagesContext);
+  const [, diffDispatch] = useContext(DiffContext);
+  const [, invDispatch] = useContext(InvestiblesContext);
+  const [operationRunning, setOperationRunning] = useContext(OperationInProgressContext);
+  const inCurrentVotingStage = getInCurrentVotingStage(marketStagesState, marketId);
+  const proposedStage = getProposedOptionsStage(marketStagesState, marketId);
 
   function getInvestibleVotes() {
     // first set every investibles support and investments to 0
@@ -90,15 +106,18 @@ function CurrentVoting(props) {
     return tallies;
   }
 
-  function getItemVote(item, index) {
+  function onDragStart(event) {
+    event.dataTransfer.setData('text', event.target.id);
+  }
+
+  function getItemVote(item) {
     const { id, investments, name } = item;
     const investibleComments = comments.filter(
       comment => comment.investible_id === id && !comment.parent_id
     );
     const myMessage = findMessageOfTypeAndId(id, messagesState);
-    const cssId = `option${index}`;
     return (
-      <Grid item id={cssId} key={id} xs={12} sm={12} md={6}>
+      <Grid item id={id} key={id} xs={12} sm={12} md={6} draggable={!operationRunning} onDragStart={onDragStart}>
         <RaisedCard
           className="raisedcard"
           onClick={() => navigate(history, formInvestibleLink(marketId, id))}
@@ -122,6 +141,24 @@ function CurrentVoting(props) {
     }
   }
 
+  function onDropApprovable(event) {
+    const investibleId = event.dataTransfer.getData('text');
+    const moveInfo = {
+      marketId,
+      investibleId,
+      stageInfo: {
+        current_stage_id: proposedStage.id,
+        stage_id: inCurrentVotingStage.id,
+      },
+    };
+    setOperationRunning(true);
+    return moveInvestibleToCurrentVoting(moveInfo)
+      .then((inv) => {
+        refreshInvestibles(invDispatch, diffDispatch, [inv]);
+        setOperationRunning(false);
+      });
+  }
+
   const tallies = getInvestibleVotes();
   const talliesArray = Object.values(tallies);
   // descending order of support
@@ -132,8 +169,9 @@ function CurrentVoting(props) {
   ).reverse();
   const { fontSize, ref } = useFitText({ maxFontSize: 200 });
   return (
-    <Grid container spacing={1}>
-      {!_.isEmpty(sortedTalliesArray) && sortedTalliesArray.map((item, index) => getItemVote(item, index))}
+    <Grid container spacing={1} onDragOver={(event) => event.preventDefault()}
+          onDrop={onDropApprovable}>
+      {!_.isEmpty(sortedTalliesArray) && sortedTalliesArray.map((item) => getItemVote(item))}
       {_.isEmpty(sortedTalliesArray) && (
         <Grid item key="noneWarning">
           <Link
--- src/pages/Dialog/Decision/ProposedIdeas.js
+++ src/pages/Dialog/Decision/ProposedIdeas.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { useHistory } from 'react-router'
+import _ from 'lodash'
 import { CardContent, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import { formInvestibleLink, navigate } from '../../../utils/marketIdPathFunctions'
 import RaisedCard from '../../../components/Cards/RaisedCard'
 import OptionCard from '../../../components/Cards/OptionCard'
+import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext'
+import { moveInvestibleBackToOptionPool } from '../../../api/investibles'
+import { refreshInvestibles } from '../../../contexts/InvestibesContext/investiblesContextHelper'
+import {
+  getInCurrentVotingStage,
+  getProposedOptionsStage
+} from '../../../contexts/MarketStagesContext/marketStagesContextHelper'
+import { MarketStagesContext } from '../../../contexts/MarketStagesContext/MarketStagesContext'
+import { DiffContext } from '../../../contexts/DiffContext/DiffContext'
+import { InvestiblesContext } from '../../../contexts/InvestibesContext/InvestiblesContext'
 
 const useStyles = makeStyles((theme) => ({
   textData: {
@@ -17,12 +28,44 @@ const useStyles = makeStyles((theme) => ({
       padding: 0
     }
   },
+  grow: {
+    padding: '30px',
+    flexGrow: 1,
+  },
 }));
 
 function ProposedIdeas(props) {
   const history = useHistory();
   const classes = useStyles();
   const { investibles, marketId } = props;
+  const [operationRunning, setOperationRunning] = useContext(OperationInProgressContext);
+  const [marketStagesState] = useContext(MarketStagesContext);
+  const [, diffDispatch] = useContext(DiffContext);
+  const [, invDispatch] = useContext(InvestiblesContext);
+  const inCurrentVotingStage = getInCurrentVotingStage(marketStagesState, marketId);
+  const proposedStage = getProposedOptionsStage(marketStagesState, marketId);
+
+  function onDragStart(event) {
+    event.dataTransfer.setData('text', event.target.id);
+  }
+
+  function onDropProposed(event) {
+    const investibleId = event.dataTransfer.getData('text');
+    const moveInfo = {
+      marketId,
+      investibleId,
+      stageInfo: {
+        current_stage_id: inCurrentVotingStage.id,
+        stage_id: proposedStage.id,
+      },
+    };
+    setOperationRunning(true);
+    return moveInvestibleBackToOptionPool(moveInfo)
+      .then((inv) => {
+        refreshInvestibles(invDispatch, diffDispatch, [inv]);
+        setOperationRunning(false);
+      });
+  }
   
   function getInvestibles() {
     return investibles.map((inv) => {
@@ -31,9 +74,10 @@ function ProposedIdeas(props) {
       return (
         <Grid
           item
-          key={id}
+          key={id} id={id}
           xs={12}
           sm={6}
+          draggable={!operationRunning} onDragStart={onDragStart}
         >
           <RaisedCard
             className="raisedcard"
@@ -50,8 +94,12 @@ function ProposedIdeas(props) {
   }
 
   return (
-    <Grid container spacing={1}>
+    <Grid container spacing={1} onDragOver={(event) => event.preventDefault()}
+          onDrop={onDropProposed}>
       {getInvestibles()}
+      {_.isEmpty(investibles) && (
+        <div className={classes.grow} />
+      )}
     </Grid>
   );
 

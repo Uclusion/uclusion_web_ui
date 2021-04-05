756d5fb286e17acc0bbb7f54fdb1b0b8084edc35
disrael <disrael@gmail.com>
2021-04-04 21:14:46 -0700

    fix: home page active stories displays issues

--- src/config/locales/en.js
+++ src/config/locales/en.js
@@ -254,7 +254,7 @@ const messages = defineMessages({
   inactive: 'Inactive',
 
   // Navigation menu
-  swimLanes: 'Assigned Stories',
+  swimLanes: 'Assigned',
   commentAddBox: 'Add Comment',
   questions: 'Questions',
   suggestions: 'Suggestions',

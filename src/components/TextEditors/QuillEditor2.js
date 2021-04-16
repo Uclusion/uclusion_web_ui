1f65aefe39e5f7ac621ead17788e7aee89129b4b
disrael <disrael@gmail.com>
2021-04-15 21:29:51 -0700

    fix: some padding to help Quill dropdowns - not a great solution

--- src/components/TextEditors/QuillEditor2.js
+++ src/components/TextEditors/QuillEditor2.js
@@ -442,7 +442,7 @@ function QuillEditor2 (props) {
         </LoadingOverlay>
       </div>
       {isTinyWindow() && <div style={{ height: '40px' }}>&nbsp;</div>}
-      <div>
+      <div style={{paddingTop: '1rem', paddingBottom: '1rem'}}>
         {children}
       </div>
     </div>

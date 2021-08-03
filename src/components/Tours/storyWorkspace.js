bebf8cf4bc9fbaced446a0d6b0173abf31e46e72
disrael <disrael@gmail.com>
2021-08-03 15:51:16 -0700

    fix: better onboarding

--- src/components/Tours/storyWorkspace.js
+++ src/components/Tours/storyWorkspace.js
@@ -5,20 +5,20 @@ export function inviteStoriesWorkspaceSteps(intl) {
     disableBeacon: true,
     target: '#workspaceMain',
     placement: 'top',
-    content: 'We put in video and links to help you get started. Edit and add whatever your team needs to know.',
+    content: 'Everything you and your team need to get started with a process for real change.',
   }];
   if (document.getElementById('navList')) {
     steps.push({
       disableBeacon: true,
       target: `#${intl.formatMessage({ id: 'swimLanes' })}`,
       placement: 'right',
-      content: 'There is a story already assigned to you to send out this Workspace.',
+      content: 'The first story beyond failed retros is already assigned to you.',
     });
     steps.push({
       disableBeacon: true,
       target: `#${intl.formatMessage({ id: 'suggestions' })}`,
       placement: 'right',
-      content: 'Your team will be notified to vote on this Suggestion about using Uclusion.',
+      content: 'This Suggestion for using Uclusion is waiting for you to add your team so they can vote.',
     });
   }
   if (document.getElementById('yellowLevelNotification')) {
@@ -26,14 +26,14 @@ export function inviteStoriesWorkspaceSteps(intl) {
       disableBeacon: true,
       target: '#yellowLevelNotification',
       placement: 'bottom',
-      content: 'Critical, urgent and, informational notifications appear up here so you know what needs to be done when.',
+      content: 'Categorized notifications are automatically sent and appear here so you know what needs to be done when.',
     });
   }
   steps.push({
     disableBeacon: true,
     target: '#helpIcon',
     placement: 'bottom',
-    content: "This takes you to the details of how Uclusion let's you do agile project management without all the meetings."
+    content: "Documentation on how Uclusion let's you do agile project management without all the meetings."
   });
   steps.push({
     disableBeacon: true,

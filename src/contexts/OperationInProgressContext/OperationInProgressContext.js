4477b7fb6a96fe887086347e0ed6742537cce8ef
disrael <disrael@gmail.com>
2021-10-11 15:50:14 -0700

    fix: rollback operation max timer - exceeded maximum depth

--- src/contexts/OperationInProgressContext/OperationInProgressContext.js
+++ src/contexts/OperationInProgressContext/OperationInProgressContext.js
@@ -7,25 +7,11 @@ function OperationInProgressProvider (props) {
   const { children } = props;
   // Default operation in progress to true for the versions fetch
   const [state, setState] = useState(true);
-  const [operationTimer, setOperationTimer] = useState(undefined);
 
   useEffect(() => {
     beginListening(setState);
     return () => {};
   }, []);
-
-  useEffect(() => {
-    // Cannot risk user being locked out if an operation doesn't complete
-    // since at that point the guard against parallel operations more dangerous than the operations
-    if (state) {
-      setOperationTimer(setInterval(() => {
-        setState(false);
-      }, 3500));
-    } else if (operationTimer) {
-      clearInterval(operationTimer);
-    }
-    return () => {};
-  }, [operationTimer, state]);
 
   return (
     <OperationInProgressContext.Provider value={[state, setState]}>

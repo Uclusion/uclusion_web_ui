db4e564185f6b51c1a352845f8adebcd5d02a089
disrael <disrael@gmail.com>
2021-12-23 11:44:22 -0800

    fix: make sure tokens loaded before loading inbox or outbox

--- src/pages/Home/YourWork/InboxFull.js
+++ src/pages/Home/YourWork/InboxFull.js
@@ -6,7 +6,10 @@ import MenuBookIcon from '@material-ui/icons/MenuBook'
 import React, { useContext } from 'react'
 import { MarketsContext } from '../../../contexts/MarketsContext/MarketsContext'
 import { MarketPresencesContext } from '../../../contexts/MarketPresencesContext/MarketPresencesContext'
-import { getNotHiddenMarketDetailsForUser } from '../../../contexts/MarketsContext/marketsContextHelper'
+import {
+  getNotHiddenMarketDetailsForUser,
+  marketTokenLoaded
+} from '../../../contexts/MarketsContext/marketsContextHelper'
 import _ from 'lodash'
 import SettingsIcon from '@material-ui/icons/Settings'
 import { Link } from '@material-ui/core'
@@ -20,9 +23,29 @@ function InboxFull(props) {
   const { hidden } = props;
   const intl = useIntl();
   const history = useHistory();
-  const [marketsState] = useContext(MarketsContext);
+  const [marketsState, , tokensHash] = useContext(MarketsContext);
   const [marketPresencesState] = useContext(MarketPresencesContext);
   const myNotHiddenMarketsState = getNotHiddenMarketDetailsForUser(marketsState, marketPresencesState);
+  let loading = marketsState.initializing;
+  if (!loading && myNotHiddenMarketsState.marketDetails) {
+    myNotHiddenMarketsState.marketDetails.forEach((market) => {
+      if (!marketTokenLoaded(market.id, tokensHash)) {
+        loading = true;
+      }
+    });
+  }
+  if (loading) {
+    // Cannot allow Quill to try to display a picture without a market token
+    return (
+      <Screen
+        hidden={hidden}
+        loading={loading}
+        title={intl.formatMessage({ id: 'loadingMessage' })}
+      >
+        <div />
+      </Screen>
+    );
+  }
   const navigationMenu = {
     showSearch: false,
     navListItemTextArray: [
--- src/pages/Home/YourWork/OutboxFull.js
+++ src/pages/Home/YourWork/OutboxFull.js
@@ -2,11 +2,40 @@ import { useIntl } from 'react-intl'
 import Screen from '../../../containers/Screen/Screen'
 import PropTypes from 'prop-types'
 import Outbox from './Outbox'
+import React, { useContext } from 'react'
+import { MarketsContext } from '../../../contexts/MarketsContext/MarketsContext'
+import { MarketPresencesContext } from '../../../contexts/MarketPresencesContext/MarketPresencesContext'
+import {
+  getNotHiddenMarketDetailsForUser,
+  marketTokenLoaded
+} from '../../../contexts/MarketsContext/marketsContextHelper'
 
 function OutboxFull(props) {
   const { hidden } = props;
   const intl = useIntl();
-
+  const [marketsState, , tokensHash] = useContext(MarketsContext);
+  const [marketPresencesState] = useContext(MarketPresencesContext);
+  const myNotHiddenMarketsState = getNotHiddenMarketDetailsForUser(marketsState, marketPresencesState);
+  let loading = marketsState.initializing;
+  if (!loading && myNotHiddenMarketsState.marketDetails) {
+    myNotHiddenMarketsState.marketDetails.forEach((market) => {
+      if (!marketTokenLoaded(market.id, tokensHash)) {
+        loading = true;
+      }
+    });
+  }
+  if (loading) {
+    // Cannot allow Quill to try to display a picture without a market token
+    return (
+      <Screen
+        hidden={hidden}
+        loading={loading}
+        title={intl.formatMessage({ id: 'loadingMessage' })}
+      >
+        <div />
+      </Screen>
+    );
+  }
   return (
     <Screen
       title={intl.formatMessage({id: 'outbox'})}

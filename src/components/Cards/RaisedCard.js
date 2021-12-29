ae3aa895cc5cf048fc4689d52170354f3e381cac
disrael <disrael@gmail.com>
2021-12-28 16:35:50 -0800

    fix: highlighting on options

--- src/components/Cards/RaisedCard.js
+++ src/components/Cards/RaisedCard.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles({
     padding: '8px',
     display: 'flex',
     flexDirection: 'column',
-    boxShadow: '4px 4px 4px red'
+    boxShadow: '0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px red'
   }
 })
 

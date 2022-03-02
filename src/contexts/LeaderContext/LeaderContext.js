74fc3852124c71c054768db53d96c0ee54c0ed2a
disrael <disrael@gmail.com>
2022-03-01 22:18:00 -0800

    fix: do not blink on initial loading because freezes buttons

--- src/contexts/LeaderContext/LeaderContext.js
+++ src/contexts/LeaderContext/LeaderContext.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useReducer, useState } from 'react'
 import { BroadcastChannel, createLeaderElection } from 'broadcast-channel'
 import reducer, { updateLeader } from './leaderContextReducer'
-import { pushMessage } from '../../utils/MessageBusUtils'
-import {
-  OPERATION_HUB_CHANNEL,
-  START_OPERATION,
-  STOP_OPERATION
-} from '../OperationInProgressContext/operationInProgressMessages'
 import { refreshNotifications, refreshVersions } from '../VersionsContext/versionsContextHelper'
 
 const EMPTY_STATE = {
@@ -39,7 +33,6 @@ function LeaderProvider(props) {
 
   useEffect(() => {
     if (channel) {
-      pushMessage(OPERATION_HUB_CHANNEL, { event: START_OPERATION });
       console.info('Applying for leader');
       // If you grab leader not signed in then you risk stalling out as no one gets data
       const elector = createLeaderElection(channel, {
@@ -52,14 +45,12 @@ function LeaderProvider(props) {
         // but its a bit risky as can somehow infinite refresh and corner of corner case anyway
         dispatch(updateLeader(isLeader));
         if (!isLeader) {
-          pushMessage(OPERATION_HUB_CHANNEL, { event: STOP_OPERATION });
           return elector.awaitLeadership().then(() => dispatch(updateLeader(true)));
         }
         // Go ahead and get the latest when determine leader
         return refreshVersions().then(() => {
           console.info('Refreshed versions from leader init');
           refreshNotifications();
-          pushMessage(OPERATION_HUB_CHANNEL, { event: STOP_OPERATION });
         });
       });
     }

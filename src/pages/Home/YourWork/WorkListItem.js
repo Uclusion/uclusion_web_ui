df2f6345bf80dd1fc80dd220a61f17e2f3195f70
disrael <disrael@gmail.com>
2022-03-10 10:40:24 -0800

    fix: again get the ampersand to kick in

--- src/pages/Home/YourWork/WorkListItem.js
+++ src/pages/Home/YourWork/WorkListItem.js
@@ -217,7 +217,7 @@ function WorkListItem(props) {
                   }
                   return navigate(history, useLink);
                 }
-              }><Description style={{ width: '55vw' }}>{fullText}</Description></a>&nbsp;
+              }><Description style={{ maxWidth: '55vw' }}>{fullText}</Description></a>&nbsp;
               {moreDescription && (
                 <Text> - {moreDescription}</Text>
               )}

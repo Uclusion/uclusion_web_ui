21e153dbe5bc9565cc4461fb669438f6d94e2b93
disrael <disrael@gmail.com>
2022-04-22 08:39:02 -0700

    fix: pending tab instead of page - prevent disappearing indicator

--- src/pages/Home/YourWork/Inbox.js
+++ src/pages/Home/YourWork/Inbox.js
@@ -253,7 +253,7 @@ function Inbox(props) {
         </Box>
       </div>
       <GmailTabs value={tabIndex} onChange={(event, value) => setTabIndex(value)}
-                 indicatorColors={[htmlColor, 'black']}
+                 indicatorColors={[htmlColor, 'black']} mobileLayout={mobileLayout}
                  style={{borderTop: '1px ridge lightgrey', paddingBottom: '0.25rem'}}>
         <GmailTabItem icon={<InboxIcon />} label={intl.formatMessage({id: 'inbox'})} color={htmlColor}
                       tag={unreadCount > 0 ? `${unreadCount} unread` : undefined} />

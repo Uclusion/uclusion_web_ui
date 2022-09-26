cf76b96d83b42fa45d801b8d5e2429bb5f930f2b
disrael <disrael@gmail.com>
2022-09-25 21:50:40 -0700

    fix: scroll and float

--- src/pages/Investible/Planning/PlanningInvestible.js
+++ src/pages/Investible/Planning/PlanningInvestible.js
@@ -1083,7 +1083,7 @@ function PlanningInvestible(props) {
           }}
           indicatorColors={['#00008B', '#00008B', '#00008B', '#00008B', '#00008B', '#00008B']}
           style={{ paddingBottom: '0.25rem', zIndex: 8, position: 'fixed', paddingTop: '0.5rem',
-            marginTop: '-15px', paddingLeft: 0, marginLeft: '-0.5rem' }}>
+            marginTop: '-15px', paddingLeft: 0, marginLeft: '-0.5rem', paddingRight: '25rem' }}>
           {(!singleTabLayout || sectionOpen === 'descriptionVotingSection') && (
             <GmailTabItem icon={<ThumbsUpDownIcon />} tagLabel={getTagLabel('votes')}
                           label={intl.formatMessage({id: 'descriptionVotingLabel'})}

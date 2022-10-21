7c7cf82efcfab64d238511243ced066c676e85af
Ben Follis <bfollis@uclusion.com>
2022-10-20 22:34:10 -0700

    Feat: Reworked Group creation to happen immediately

--- src/components/AddNewWizards/Group/GroupNameStep.js
+++ src/components/AddNewWizards/Group/GroupNameStep.js
@@ -6,19 +6,51 @@ import _ from 'lodash'
 import WizardStepContainer from '../WizardStepContainer';
 import { WizardStylesContext } from '../WizardStylesContext';
 import WizardStepButtons from '../WizardStepButtons';
+import { doCreateGroup } from './groupCreator'
+import { formMarketLink } from '../../../utils/marketIdPathFunctions'
+import { MarketGroupsContext } from '../../../contexts/MarketGroupsContext/MarketGroupsContext'
+import { DiffContext } from '../../../contexts/DiffContext/DiffContext'
+import { GroupMembersContext } from '../../../contexts/GroupMembersContext/GroupMembersContext'
 
 function GroupNameStep (props) {
-  const { updateFormData, formData } = props;
+  const { updateFormData, formData, marketId } = props;
   const intl = useIntl();
   const value = formData.name || '';
   const validForm = !_.isEmpty(value);
   const classes = useContext(WizardStylesContext);
+
+  const [, groupsDispatch] = useContext(MarketGroupsContext);
+  const [, diffDispatch] = useContext(DiffContext);
+  const [, groupMembersDispatch] = useContext(GroupMembersContext);
 
   function onNameChange (event) {
     const { value } = event.target;
     updateFormData({
       name: value
     });
+  }
+
+  function onNext(){
+      const dispatchers = {
+        groupsDispatch,
+        diffDispatch,
+        groupMembersDispatch
+      };
+      // default things not filled in
+      const groupData = {
+        ...formData,
+        marketId,
+        votesRequired: formData.votesRequired ?? 0,
+      };
+      return doCreateGroup(dispatchers, groupData)
+        .then((group) => {
+          const {id: groupId} = group;
+          const link = formMarketLink(marketId, groupId);
+          updateFormData({
+            link,
+            groupId,
+          })
+        });
   }
 
   return (
@@ -46,6 +78,7 @@ function GroupNameStep (props) {
       <WizardStepButtons
         {...props}
         validForm={validForm}
+        onNext={onNext}
         nextLabel="GroupWizardAddMembers"
         showTerminate={true}
         terminateLabel="GroupWizardGotoGroup"/>

036c676ce6df79ddd31aa4df6212afae7b4c625c
disrael <disrael@gmail.com>
2022-11-12 15:22:15 -0800

    fix: show assigned as part of job desc

--- src/components/InboxWizards/JobDescription.js
+++ src/components/InboxWizards/JobDescription.js
@@ -5,13 +5,24 @@ import { useInvestibleEditStyles } from '../../pages/Investible/InvestibleBodyEd
 import { getInvestible } from '../../contexts/InvestibesContext/investiblesContextHelper'
 import { InvestiblesContext } from '../../contexts/InvestibesContext/InvestiblesContext'
 import { WizardStylesContext } from './WizardStylesContext'
+import GravatarGroup from '../Avatars/GravatarGroup'
+import { getMarketInfo } from '../../utils/userFunctions'
+import { getMarketPresences } from '../../contexts/MarketPresencesContext/marketPresencesHelper'
+import { MarketPresencesContext } from '../../contexts/MarketPresencesContext/MarketPresencesContext'
+import { useIntl } from 'react-intl'
 
 function JobDescription(props) {
-  const { investibleId } = props;
+  const { investibleId, marketId } = props;
+  const intl = useIntl();
   const investibleEditClasses = useInvestibleEditStyles();
   const [investiblesState] = useContext(InvestiblesContext);
   const classes = useContext(WizardStylesContext);
+  const [marketPresencesState] = useContext(MarketPresencesContext);
   const inv = getInvestible(investiblesState, investibleId);
+  const marketInfo = getMarketInfo(inv, marketId) || {};
+  const { assigned } = marketInfo || {};
+  const marketPresences = getMarketPresences(marketPresencesState, marketId) || [];
+  const assignedPresences = marketPresences.filter((presence) => (assigned || []).includes(presence.id))
   const { investible: myInvestible } = inv || {};
   const { name, description } = myInvestible || {};
 
@@ -19,6 +30,11 @@ function JobDescription(props) {
     <>
       <div style={{maxHeight: '300px', minHeight: '200px', overflowY: 'auto', overflowX: 'hidden', paddingLeft: '4px',
         paddingRight: '4px', paddingTop: '1rem'}}>
+        <div style={{alignItems: 'center', display: 'flex', paddingBottom: '1rem'}}>
+          <Typography variant='body2' style={{ paddingRight: '0.5rem'}}>
+            {intl.formatMessage({ id: 'planningInvestibleAssignments' })}</Typography>
+          <GravatarGroup users={assignedPresences} gravatarClassName={classes.smallGravatar} />
+        </div>
         <Typography className={investibleEditClasses.title} variant="h3" component="h1">
           {name}
         </Typography>

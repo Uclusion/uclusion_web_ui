f470a96e455d75d84b4ddbacbe8660ecc52ba23a
disrael <disrael@gmail.com>
2023-01-16 11:47:42 -0800

    feat: intermediate sizes

--- src/components/Search/SearchBox.js
+++ src/components/Search/SearchBox.js
@@ -118,7 +118,7 @@ function SearchBox () {
   return (
     <div id='search-box' onClick={(event) => event.stopPropagation()} style={{flex: 1}}>
       <TextField
-        style={{backgroundColor: '#e5edee', width: '100%'}}
+        style={{backgroundColor: '#e5edee', width: '100%', minWidth: '10rem'}}
         onChange={onSearchChange}
         onKeyPress={(ev) => {
           if (ev.key === 'Enter') {

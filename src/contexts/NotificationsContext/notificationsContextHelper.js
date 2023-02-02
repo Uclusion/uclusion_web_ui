7f7f9932604aa434ad283f881de70913e4ceec1d
disrael <disrael@gmail.com>
2023-02-01 20:40:20 -0800

    fix: show vote on your own question - that wizard will have to be fixed anyway

--- src/contexts/NotificationsContext/notificationsContextHelper.js
+++ src/contexts/NotificationsContext/notificationsContextHelper.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import { DECISION_TYPE } from '../../constants/markets'
 import { getMarket } from '../MarketsContext/marketsContextHelper'
 import { getMarketPresences } from '../MarketPresencesContext/marketPresencesHelper'
 import { getInvestible } from '../InvestibesContext/investiblesContextHelper'
@@ -77,18 +76,7 @@ export function isInInbox(message, marketState, marketPresencesState, commentsSt
     console.warn(message);
     return false;
   }
-  if (!message.type || message.type === 'UNREAD_REPORT' || message.deleted) {
-    return false;
-  }
-  if (message.type === 'NOT_FULLY_VOTED' && message.market_type === DECISION_TYPE) {
-    // Display the need to vote in pending or else too confusing and disappears too quickly after vote
-    // Also its your question so if you don't want to vote no pressure
-    const market = getMarket(marketState, message.market_id) || {};
-    const anInlineMarketPresences = getMarketPresences(marketPresencesState, message.market_id) || [];
-    const yourPresence = anInlineMarketPresences.find((presence) => presence.current_user) || {};
-    return market.created_by !== yourPresence.id;
-  }
-  return true;
+  return !(!message.type || message.type === 'UNREAD_REPORT' || message.deleted);
 }
 
 export function getInboxTarget() {

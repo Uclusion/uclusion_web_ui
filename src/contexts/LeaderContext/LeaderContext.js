3797c17b213fa848ba9099fa9283396705af67db
Ben Follis <bfollis@uclusion.com>
2023-03-06 22:39:21 -0800

    Fixed leader election to be really fast

--- src/contexts/LeaderContext/LeaderContext.js
+++ src/contexts/LeaderContext/LeaderContext.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useReducer, useState } from 'react'
-import { BroadcastChannel, createLeaderElection } from 'broadcast-channel'
+import React, { useEffect, useReducer } from 'react'
+import { waitForLeadership } from 'tab-election';
 import reducer, { updateLeader } from './leaderContextReducer'
 import { refreshVersions } from '../../api/versionedFetchUtils'
 
@@ -12,41 +12,16 @@ const LeaderContext = React.createContext(EMPTY_STATE);
 function LeaderProvider(props) {
   const { children, authState, userId } = props;
   const [state, dispatch] = useReducer(reducer, EMPTY_STATE);
-  const [channel, setChannel] = useState(undefined);
+
 
 
   useEffect(() => {
-    let myChannel;
     if (authState === 'signedIn' && userId) {
-      console.info(`Processing leader with userId ${userId}`);
-      myChannel = new BroadcastChannel(userId);
-      setChannel(myChannel);
-    }
-    return () => {
-      if (myChannel) {
-        console.info('Closing channel');
-        myChannel.close();
-        setChannel(undefined);
-      }
-    };
-  }, [authState, userId]);
-
-  useEffect(() => {
-    if (channel) {
-      console.info('Applying for leader');
-      // If you grab leader not signed in then you risk stalling out as no one gets data
-      const elector = createLeaderElection(channel, {
-        fallbackInterval: 5000, // optional configuration for how often will renegotiation for leader occur
-        responseTime: 5000, // optional configuration for how long will instances have to respond
-      });
-      return elector.applyOnce().then((isLeader) => {
-        console.info(`Setting leader to ${isLeader}`);
+      waitForLeadership(() => {
+        console.info(`Claiming leadership`);
         // Could use broadcast ID to send message out to others to refresh out of login page
         // but its a bit risky as can somehow infinite refresh and corner of corner case anyway
-        dispatch(updateLeader(isLeader));
-        if (!isLeader) {
-          return elector.awaitLeadership().then(() => dispatch(updateLeader(true)));
-        }
+        dispatch(updateLeader(true));
         // Go ahead and get the latest when determine leader
         return refreshVersions(true).then(() => {
           console.info('Refreshed versions from leader init');
@@ -54,7 +29,7 @@ function LeaderProvider(props) {
       });
     }
     return () => {};
-  }, [channel]);
+  }, [authState, userId]);
 
   return (
     <LeaderContext.Provider value={[state, dispatch]}>

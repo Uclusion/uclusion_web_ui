19e91607b5a1d1e28792b4b15788fe469fbe3d87
disrael <disrael@gmail.com>
2023-03-23 11:19:36 -0700

    fix: seperate navigation and defer/delete buttons

--- src/components/InboxWizards/Answer/DecideAnswerStep.js
+++ src/components/InboxWizards/Answer/DecideAnswerStep.js
@@ -1,54 +1,37 @@
-import React, { useContext } from 'react'
-import PropTypes from 'prop-types'
-import { Typography } from '@material-ui/core'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
 import WizardStepContainer from '../WizardStepContainer';
-import { wizardStyles } from '../WizardStylesContext'
+import { wizardStyles } from '../WizardStylesContext';
 import WizardStepButtons from '../WizardStepButtons';
-import CommentBox from '../../../containers/CommentBox/CommentBox'
-import { getCommentRoot } from '../../../contexts/CommentsContext/commentsContextHelper'
-import { CommentsContext } from '../../../contexts/CommentsContext/CommentsContext'
-import { getInvestible } from '../../../contexts/InvestibesContext/investiblesContextHelper'
-import { InvestiblesContext } from '../../../contexts/InvestibesContext/InvestiblesContext'
-import { getMarketInfo } from '../../../utils/userFunctions'
-import { getFullStage } from '../../../contexts/MarketStagesContext/marketStagesContextHelper'
-import { MarketStagesContext } from '../../../contexts/MarketStagesContext/MarketStagesContext'
-import { marketAbstain } from '../../../api/markets'
-import { changeMyPresence } from '../../../contexts/MarketPresencesContext/marketPresencesHelper'
-import { removeMessagesForCommentId } from '../../../utils/messageUtils'
-import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext'
-import { useHistory } from 'react-router'
-import { wizardFinish } from '../InboxWizardUtils'
-import { formCommentLink } from '../../../utils/marketIdPathFunctions'
-import { MarketPresencesContext } from '../../../contexts/MarketPresencesContext/MarketPresencesContext'
-import { NotificationsContext } from '../../../contexts/NotificationsContext/NotificationsContext'
-import { workListStyles } from '../../../pages/Home/YourWork/WorkListItem'
+import CommentBox from '../../../containers/CommentBox/CommentBox';
+import { getCommentRoot } from '../../../contexts/CommentsContext/commentsContextHelper';
+import { CommentsContext } from '../../../contexts/CommentsContext/CommentsContext';
+import { marketAbstain } from '../../../api/markets';
+import { changeMyPresence } from '../../../contexts/MarketPresencesContext/marketPresencesHelper';
+import { removeMessagesForCommentId } from '../../../utils/messageUtils';
+import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext';
+import { MarketPresencesContext } from '../../../contexts/MarketPresencesContext/MarketPresencesContext';
+import { NotificationsContext } from '../../../contexts/NotificationsContext/NotificationsContext';
+import { removeWorkListItem, workListStyles } from '../../../pages/Home/YourWork/WorkListItem';
 import { useIntl } from 'react-intl';
-
+import JobDescription from '../JobDescription';
 
 function DecideAnswerStep(props) {
   const { marketId, commentId, clearFormData, message } = props;
   const [commentState] = useContext(CommentsContext);
-  const [investibleState] = useContext(InvestiblesContext);
-  const [marketStagesState] = useContext(MarketStagesContext);
   const [, setOperationRunning] = useContext(OperationInProgressContext);
   const [marketPresencesState, presenceDispatch] = useContext(MarketPresencesContext);
   const [messagesState, messagesDispatch] = useContext(NotificationsContext);
-  const history = useHistory();
   const commentRoot = getCommentRoot(commentState, marketId, commentId) || {id: 'fake'};
   const comments = (commentState[marketId] || []).filter((comment) =>
     comment.root_comment_id === commentRoot.id || comment.id === commentRoot.id);
   const classes = wizardStyles();
   const intl = useIntl();
   const workItemClasses = workListStyles();
-  const inv = commentRoot.investible_id ? getInvestible(investibleState, commentRoot.investible_id) : undefined;
-  const marketInfo = getMarketInfo(inv, marketId) || {};
-  const { stage } = marketInfo;
-  const fullStage = getFullStage(marketStagesState, marketId, stage) || {};
 
   function myOnFinish() {
-    wizardFinish({link: formCommentLink(marketId, commentRoot.group_id, commentRoot.investible_id,
-          commentRoot.id)},
-      setOperationRunning, message, history, marketId, commentRoot.investible_id, messagesDispatch);
+    removeWorkListItem(message, workItemClasses.removed, messagesDispatch);
   }
 
   function abstain() {
@@ -73,21 +56,23 @@ function DecideAnswerStep(props) {
       <Typography className={classes.introText}>
         {intl.formatMessage({id: 'DecideAnswerTitle'})}
       </Typography>
-      <div className={classes.wizardCommentBoxDiv}>
-        <CommentBox
-          comments={comments}
-          marketId={marketId}
-          allowedTypes={[]}
-          fullStage={fullStage}
-          investible={inv}
-          marketInfo={marketInfo}
-          isInbox
-          removeActions
-        />
-      </div>
+      {commentRoot.investible_id && (
+        <JobDescription marketId={marketId} investibleId={commentRoot.investible_id} comments={comments} />
+      )}
+      {!commentRoot.investible_id && (
+        <div className={classes.wizardCommentBoxDiv}>
+          <CommentBox
+            comments={comments}
+            marketId={marketId}
+            allowedTypes={[]}
+            isInbox
+            removeActions
+          />
+        </div>
+      )}
       <WizardStepButtons
         {...props}
-        nextLabel="DecideAnswerTerminate"
+        nextLabel="defer"
         onNext={myOnFinish}
         spinOnClick={false}
         showOtherNext

73c2ad3e02f9e62abed837453cb62491482384e9
disrael <disrael@gmail.com>
2023-03-22 19:51:53 -0700

    fix: job description with single comment over just single comment

--- src/components/InboxWizards/AcceptReject/DecideAcceptRejectStep.js
+++ src/components/InboxWizards/AcceptReject/DecideAcceptRejectStep.js
@@ -1,32 +1,24 @@
-import React, { useContext } from 'react'
-import PropTypes from 'prop-types'
-import { Typography } from '@material-ui/core'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
 import WizardStepContainer from '../WizardStepContainer';
-import { wizardStyles } from '../WizardStylesContext'
+import { wizardStyles } from '../WizardStylesContext';
 import WizardStepButtons from '../WizardStepButtons';
-import CommentBox from '../../../containers/CommentBox/CommentBox'
-import { addCommentToMarket, getCommentRoot } from '../../../contexts/CommentsContext/commentsContextHelper'
-import { CommentsContext } from '../../../contexts/CommentsContext/CommentsContext'
-import { getInvestible } from '../../../contexts/InvestibesContext/investiblesContextHelper'
-import { InvestiblesContext } from '../../../contexts/InvestibesContext/InvestiblesContext'
-import { getMarketInfo } from '../../../utils/userFunctions'
-import { getFullStage } from '../../../contexts/MarketStagesContext/marketStagesContextHelper'
-import { MarketStagesContext } from '../../../contexts/MarketStagesContext/MarketStagesContext'
-import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext'
-import { useHistory } from 'react-router'
-import { wizardFinish } from '../InboxWizardUtils'
-import { formCommentLink } from '../../../utils/marketIdPathFunctions'
-import { removeWorkListItem, workListStyles } from '../../../pages/Home/YourWork/WorkListItem'
-import { resolveComment, updateComment } from '../../../api/comments'
-import { TODO_TYPE } from '../../../constants/comments'
+import { addCommentToMarket, getCommentRoot } from '../../../contexts/CommentsContext/commentsContextHelper';
+import { CommentsContext } from '../../../contexts/CommentsContext/CommentsContext';
+import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext';
+import { useHistory } from 'react-router';
+import { wizardFinish } from '../InboxWizardUtils';
+import { formCommentLink } from '../../../utils/marketIdPathFunctions';
+import { removeWorkListItem, workListStyles } from '../../../pages/Home/YourWork/WorkListItem';
+import { resolveComment, updateComment } from '../../../api/comments';
+import { TODO_TYPE } from '../../../constants/comments';
 import { NotificationsContext } from '../../../contexts/NotificationsContext/NotificationsContext';
-
+import JobDescription from '../JobDescription';
 
 function DecideAcceptRejectStep(props) {
   const { marketId, commentId, message } = props;
   const [commentState] = useContext(CommentsContext);
-  const [investibleState] = useContext(InvestiblesContext);
-  const [marketStagesState] = useContext(MarketStagesContext);
   const [, setOperationRunning] = useContext(OperationInProgressContext);
   const [commentsState, commentsDispatch] = useContext(CommentsContext);
   const [, messagesDispatch] = useContext(NotificationsContext);
@@ -36,12 +28,6 @@ function DecideAcceptRejectStep(props) {
     comment.root_comment_id === commentRoot.id || comment.id === commentRoot.id);
   const classes = wizardStyles();
   const workItemClasses = workListStyles();
-  const inv = commentRoot.investible_id ? getInvestible(investibleState, commentRoot.investible_id) : {} ;
-  const { investible: myInvestible } = inv;
-  const { name } = myInvestible || {};
-  const marketInfo = getMarketInfo(inv, marketId) || {};
-  const { stage } = marketInfo;
-  const fullStage = getFullStage(marketStagesState, marketId, stage) || {};
 
   function myOnFinish() {
     wizardFinish({link: formCommentLink(marketId, commentRoot.group_id, commentRoot.investible_id,
@@ -72,20 +58,9 @@ function DecideAcceptRejectStep(props) {
     >
     <div>
       <Typography className={classes.introText}>
-        Do you accept this suggestion for "{name}"?
+        Do you accept this suggestion?
       </Typography>
-      <div className={classes.wizardCommentBoxDiv}>
-        <CommentBox
-          comments={comments}
-          marketId={marketId}
-          allowedTypes={[]}
-          fullStage={fullStage}
-          investible={inv}
-          marketInfo={marketInfo}
-          isInbox
-          removeActions
-        />
-      </div>
+      <JobDescription marketId={marketId} investibleId={commentRoot.investible_id} comments={comments} />
       <WizardStepButtons
         {...props}
         onFinish={myOnFinish}

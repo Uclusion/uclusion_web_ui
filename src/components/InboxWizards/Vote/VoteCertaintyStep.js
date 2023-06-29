93b32d3a3a6dd7808a6481ffaeb0c6b48a234b6a
disrael <disrael@gmail.com>
2023-06-29 10:13:30 -0700

    fix: use correct ids on suggestion voting

--- src/components/InboxWizards/Vote/VoteCertaintyStep.js
+++ src/components/InboxWizards/Vote/VoteCertaintyStep.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types'
 import { Typography } from '@material-ui/core'
 import WizardStepContainer from '../WizardStepContainer'
 import { wizardStyles } from '../WizardStylesContext'
 import AddEditVote from '../../../pages/Investible/Voting/AddEditVote'
+import { getMarketInvestibles } from '../../../contexts/InvestibesContext/investiblesContextHelper';
+import _ from 'lodash';
+import { InvestiblesContext } from '../../../contexts/InvestibesContext/InvestiblesContext';
 
 function VoteCertaintyStep(props) {
-  const { marketId, formData, updateFormData, clearFormData, commentRoot, message } = props;
+  const { formData, updateFormData, clearFormData, commentRoot, message } = props;
   const classes = wizardStyles();
+  const [investiblesState] = useContext(InvestiblesContext);
   const { isFor } = formData;
+  const { inline_market_id: inlineMarketId } = commentRoot;
+  const investibles = getMarketInvestibles(investiblesState, inlineMarketId);
 
   return (
     <WizardStepContainer
@@ -18,20 +24,22 @@ function VoteCertaintyStep(props) {
         <Typography className={classes.introText} variant="h6">
           How certain are you of voting {isFor ? 'for' : 'against'} this suggestion?
         </Typography>
-        <AddEditVote
-          marketId={marketId}
-          wizardProps={props}
-          investibleId={commentRoot.investible_id}
-          groupId={commentRoot.group_id}
-          hasVoted={false}
-          allowMultiVote={false}
-          multiplier={isFor ? 1 : -1}
-          formData={formData}
-          updateFormData={updateFormData}
-          clearFormData={clearFormData}
-          voteMessage={message}
-          isInbox={true}
-        />
+        {!_.isEmpty(investibles) && (
+          <AddEditVote
+            marketId={inlineMarketId}
+            wizardProps={props}
+            investibleId={investibles[0].investible.id}
+            groupId={inlineMarketId}
+            hasVoted={false}
+            allowMultiVote={false}
+            multiplier={isFor ? 1 : -1}
+            formData={formData}
+            updateFormData={updateFormData}
+            clearFormData={clearFormData}
+            voteMessage={message}
+            isInbox={true}
+          />
+        )}
       </div>
     </WizardStepContainer>
   )

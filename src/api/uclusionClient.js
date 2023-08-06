b34d4b1681a3ab8e396ddb2db88589bad2923cdb
Ben Follis <bfollis@uclusion.com>
2023-08-05 23:43:55 -0700

    Fix: Forgot code relocation

--- src/api/uclusionClient.js
+++ src/api/uclusionClient.js
@@ -4,10 +4,10 @@ import TokenFetcher from '../authorization/TokenFetcher'
 import { TOKEN_TYPE_ACCOUNT, TOKEN_TYPE_MARKET, TOKEN_TYPE_MARKET_INVITE } from '../authorization/TokenStorageManager'
 import AmplifyIdentityTokenRefresher from '../authorization/AmplifyIdentityTokenRefresher'
 
-const AMPLIFY_IDENTITY_SOURCE = new AmplifyIdentityTokenRefresher();
-const ACCOUNT_TOKEN_FETCHER = new TokenFetcher(AMPLIFY_IDENTITY_SOURCE, SSO_CLIENT, TOKEN_TYPE_ACCOUNT, 'home_account');
 
 const SSO_CLIENT = client.getResolvedSSOClient(config.api_configuration);
+const AMPLIFY_IDENTITY_SOURCE = new AmplifyIdentityTokenRefresher();
+const ACCOUNT_TOKEN_FETCHER = new TokenFetcher(AMPLIFY_IDENTITY_SOURCE, SSO_CLIENT, TOKEN_TYPE_ACCOUNT, 'home_account');
 export const MARKET_TOKEN_FETCHER = new TokenFetcher(AMPLIFY_IDENTITY_SOURCE, SSO_CLIENT, TOKEN_TYPE_MARKET);
 
 export const getMarketClient = (marketId) => {

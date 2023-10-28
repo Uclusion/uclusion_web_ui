2c6fd6e936bc8cea378cab78d64ab78e4ef76a94
disrael <disrael@gmail.com>
2023-10-28 14:09:17 -0700

    fix: resume draft question if stopped while adding options

--- src/components/AddNewWizards/JobComment/JobCommentWizard.js
+++ src/components/AddNewWizards/JobComment/JobCommentWizard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { WizardStylesProvider } from '../WizardStylesContext';
 import FormdataWizard from 'react-formdata-wizard';
@@ -6,15 +6,29 @@ import AddCommentStep from './AddCommentStep';
 import { QUESTION_TYPE, SUGGEST_CHANGE_TYPE } from '../../../constants/comments';
 import AddOptionStep from './AddOptionStep';
 import ConfigureCommentStep from '../ConfigureCommentStep';
+import { CommentsContext } from '../../../contexts/CommentsContext/CommentsContext';
 
 function JobCommentWizard(props) {
   const { investibleId, marketId, commentType } = props;
+  const [commentsState] = useContext(CommentsContext);
+  const isQuestion = commentType === QUESTION_TYPE;
+  const investibleComments = (commentsState[marketId]||[]).filter(comment =>
+    comment.investible_id === investibleId) || [];
+  const savedQuestion = investibleComments.find((comment) => {
+    return comment.comment_type === QUESTION_TYPE && !comment.resolved && !comment.deleted && !comment.is_sent;
+  });
+  const hasDraft = isQuestion && savedQuestion;
+  const draftData = {inlineMarketId: savedQuestion?.inline_market_id, commentId: savedQuestion?.id, groupId:
+    savedQuestion?.group_id, marketId, investibleId };
 
   return (
     <WizardStylesProvider>
-      <FormdataWizard name={`job_comment_wizard${investibleId}`} useLocalStorage={false}>
-        <AddCommentStep investibleId={investibleId} marketId={marketId} useType={commentType} />
-        {commentType === QUESTION_TYPE && (
+      <FormdataWizard name={`job_comment_wizard${investibleId}`} useLocalStorage={false}
+                      defaultFormData={hasDraft ? draftData : undefined}>
+        {!hasDraft && (
+          <AddCommentStep investibleId={investibleId} marketId={marketId} useType={commentType} />
+        )}
+        {isQuestion && (
           <AddOptionStep investibleId={investibleId} marketId={marketId} />
         )}
         {[QUESTION_TYPE, SUGGEST_CHANGE_TYPE].includes(commentType) && (

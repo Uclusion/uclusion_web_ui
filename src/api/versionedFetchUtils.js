62684be20cbc1baa6a8ceecb0950d4bf543cdb4e
disrael <disrael@gmail.com>
2024-04-19 14:13:10 -0700

    fix: debug for signature matching

--- src/api/versionedFetchUtils.js
+++ src/api/versionedFetchUtils.js
@@ -215,7 +215,6 @@ export async function doVersionRefresh() {
       const { id, unmatched: signatures } = fullSignature;
       const unmatched = [];
       signatures.forEach((signature) => {
-        console.debug(signature)
         if (!checkServerSignaturesInStorage(id, {[signatureType]: [signature]}, storageStates)) {
           unmatched.push(signature);
           if (!failedMarketList.includes(id)) {

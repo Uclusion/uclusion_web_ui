cfe073a0460e6899b0abd4d7eaeeeaf493edfe34
disrael <disrael@gmail.com>
2024-05-04 10:30:18 -0700

    fix: mobile compose wizard banner

--- src/components/Banners/AddWizardOnboardingBanner.js
+++ src/components/Banners/AddWizardOnboardingBanner.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/styles';
 import Link from '@material-ui/core/Link';
 import { COMPOSE_WIZARD_TYPE } from '../../constants/markets';
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles((theme) => {
   return {
     bannerBackground: {
       background: 'white',
@@ -14,12 +14,14 @@ const useStyles = makeStyles(() => {
       '& button': {
         fontWeight: 'bold'
       },
-      width: '70%',
       display: 'flex',
       justifyContent: 'space-around'
     },
     ctaSub: {
       fontWeight: 'normal',
+      [theme.breakpoints.down('sm')]: {
+        marginTop: '0.5rem'
+      },
     }
   };
 });
@@ -36,8 +38,9 @@ function AddWizardOnboardingBanner(props) {
     <div className={classes.bannerBackground}>
       <div className={classes.bannerBox}>
           <div style={{marginTop: '0.8rem'}} id='composeDemoBannerText'>
-            <Typography><b>Enjoying the demo?</b> A <Link href="https://documentation.uclusion.com/workspaces" target="_blank">workspace</Link> is
-            the place for all developer communication and everyone in it can see all contents.</Typography>
+            <Typography><b>Enjoying the demo?</b> Everyone in
+              a <Link href="https://documentation.uclusion.com/workspaces" target="_blank">workspace</Link> can see all
+              contents.</Typography>
             <Typography className={classes.ctaSub}>
               A job is the only assignable workspace type and also the only one that can contain
               all <Link href="https://documentation.uclusion.com/structured-comments" target="_blank">structured comments</Link>.

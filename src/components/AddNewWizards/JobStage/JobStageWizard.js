3114fecd8b64cea69c9dc70cdac538c8b9fa8c4c
disrael <disrael@gmail.com>
2024-07-19 15:36:19 -0700

    fix: default stage when can

--- src/components/AddNewWizards/JobStage/JobStageWizard.js
+++ src/components/AddNewWizards/JobStage/JobStageWizard.js
@@ -10,6 +10,7 @@ import { getMarketInfo } from '../../../utils/userFunctions';
 import { InvestiblesContext } from '../../../contexts/InvestibesContext/InvestiblesContext';
 import _ from 'lodash';
 import {
+  getInCurrentVotingStage,
   isAcceptedStage, isFurtherWorkStage,
   isInReviewStage,
   isNotDoingStage
@@ -26,6 +27,7 @@ import { getMarketComments } from '../../../contexts/CommentsContext/commentsCon
 import { CommentsContext } from '../../../contexts/CommentsContext/CommentsContext';
 import StageActionStep from './StageActionStep';
 import { editorEmpty } from '../../TextEditors/Utilities/CoreUtils';
+import { MarketStagesContext } from '../../../contexts/MarketStagesContext/MarketStagesContext';
 
 function JobStageWizard(props) {
   const { marketId, investibleId, stageId, isAssign } = props;
@@ -33,6 +35,7 @@ function JobStageWizard(props) {
   const [investibleState] = useContext(InvestiblesContext);
   const [marketPresencesState] = useContext(MarketPresencesContext);
   const [commentsState] = useContext(CommentsContext);
+  const [marketStagesState] = useContext(MarketStagesContext);
   const inv = getInvestible(investibleState, investibleId);
   const marketInfo = getMarketInfo(inv, marketId) || {};
   const { stage, group_id: groupId } = marketInfo;
@@ -43,6 +46,8 @@ function JobStageWizard(props) {
   const marketComments = getMarketComments(commentsState, marketId, groupId);
   const yourReason = getReasonForVote(yourVote, marketComments);
   const isSingleUser = isSingleUserMarket(marketPresences);
+  const useStageId = stageId ? stageId :
+    (isAssign === 'true' ? getInCurrentVotingStage(marketStagesState, marketId)?.id : undefined);
 
   if (_.isEmpty(stage)) {
     return React.Fragment;
@@ -85,7 +90,7 @@ function JobStageWizard(props) {
       <FormdataWizard name={`job_stage_wizard${investibleId}`} useLocalStorage={false}
                       defaultFormData={{approveQuantity: Math.abs(approveQuantity), originalQuantity: approveQuantity,
                         wasDeleted: yourVote?.deleted, userId: yourPresence?.id, approveReason: originalReason,
-                        originalReason, stage: stageId ? stageId : undefined, stageWasSet: !!stageId,
+                        originalReason, stage: useStageId ? useStageId : undefined, stageWasSet: !!useStageId,
                         useCompression: true}}>
         {!stageId && (
           <JobStageStep myFinish={finish} marketId={marketId} investibleId={investibleId} marketInfo={marketInfo}

ea6c738f86d1697f073a7ffd934890dec1613dcb
disrael <disrael@gmail.com>
2024-08-04 15:08:34 -0700

    fix: show people in workspace

--- src/pages/Home/WorkspaceMenu.js
+++ src/pages/Home/WorkspaceMenu.js
@@ -268,7 +268,7 @@ function WorkspaceMenu(props) {
           </ProSidebar>
         </Menu>
       )}
-      {_.size(marketPresences) < 10 && _.size(marketPresences) > 1 && (
+      {_.size(presencesFiltered) < 10 && _.size(presencesFiltered) > 1 && (
         <List
           dense
           id="addressBook"

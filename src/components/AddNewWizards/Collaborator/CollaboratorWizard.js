e6c8a83cb007726f6c11aacdc3a80d6b0d72db7c
disrael <disrael@gmail.com>
2025-04-29 10:12:33 -0700

    fix: prompt for new view if first collaborator and don't have team one

--- src/components/AddNewWizards/Collaborator/CollaboratorWizard.js
+++ src/components/AddNewWizards/Collaborator/CollaboratorWizard.js
@@ -1,9 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types'
 import { WizardStylesProvider } from '../WizardStylesContext'
 import FormdataWizard from 'react-formdata-wizard'
 import { extractUsersList } from '../../../utils/userFunctions';
-import { getMarketPresences } from '../../../contexts/MarketPresencesContext/marketPresencesHelper';
+import {
+  getGroupPresences,
+  getMarketPresences,
+  isAutonomousGroup
+} from '../../../contexts/MarketPresencesContext/marketPresencesHelper';
 import { MarketPresencesContext } from '../../../contexts/MarketPresencesContext/MarketPresencesContext';
 import { MarketsContext } from '../../../contexts/MarketsContext/MarketsContext';
 import { formMarketLink, navigate } from '../../../utils/marketIdPathFunctions';
@@ -12,15 +16,27 @@ import _ from 'lodash'
 import FromOtherWorkspacesStep from './FromOtherWorkspacesStep';
 import InviteByEmailStep from './InviteByEmailStep';
 import AssignViewsStep from './AssignViewsStep';
+import { MarketGroupsContext } from '../../../contexts/MarketGroupsContext/MarketGroupsContext';
+import { GroupMembersContext } from '../../../contexts/GroupMembersContext/GroupMembersContext';
+import CreateTeamViewStep from './CreateTeamViewStep';
 
 function CollaboratorWizard (props) {
   const { marketId } = props;
   const history = useHistory();
   const [marketPresencesState] = useContext(MarketPresencesContext);
   const [marketState] = useContext(MarketsContext);
+  const [groupsState] = useContext(MarketGroupsContext);
+  const [groupPresencesState] = useContext(GroupMembersContext);
+  const [viewCreated, setViewCreated] = useState(false);
   const marketPresences = getMarketPresences(marketPresencesState, marketId) || [];
   const participants = Object.values(extractUsersList(marketPresencesState, marketState, marketPresences));
   const displayFromOther = !_.isEmpty(participants);
+  const teamGroups = groupsState[marketId].filter((group) => {
+    const groupPresences = getGroupPresences(marketPresences, groupPresencesState, marketId,
+      group.id) || [];
+    return !isAutonomousGroup(groupPresences, group);
+  });
+  const allAutonomousViews = _.isEmpty(teamGroups);
 
   const onFinish = () => {
     navigate(history, formMarketLink(marketId, marketId));
@@ -30,6 +46,9 @@ function CollaboratorWizard (props) {
     <WizardStylesProvider>
       <FormdataWizard name="collaborator_wizard" onFinish={onFinish} useLocalStorage={false}
                       defaultFormData={{groupIdIndex: 0}}>
+        {(allAutonomousViews || viewCreated) && (
+          <CreateTeamViewStep marketId={marketId} setViewCreated={setViewCreated} />
+        )}
         <InviteByEmailStep marketId={marketId} displayFromOther={displayFromOther} />
         {displayFromOther && (
           <FromOtherWorkspacesStep marketId={marketId} participants={participants}/>
---
+++ src/components/AddNewWizards/Collaborator/CreateTeamViewStep.js
@@ -0,0 +1,99 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { InputAdornment, OutlinedInput, Typography } from '@material-ui/core';
+import WizardStepContainer from '../WizardStepContainer';
+import { WizardStylesContext } from '../WizardStylesContext';
+import WizardStepButtons from '../WizardStepButtons';
+import { MarketGroupsContext } from '../../../contexts/MarketGroupsContext/MarketGroupsContext';
+import _ from 'lodash';
+import { OperationInProgressContext } from '../../../contexts/OperationInProgressContext/OperationInProgressContext';
+import { GroupMembersContext } from '../../../contexts/GroupMembersContext/GroupMembersContext';
+import { useIntl } from 'react-intl';
+import { DiffContext } from '../../../contexts/DiffContext/DiffContext';
+import { doCreateGroup } from '../Group/groupCreator';
+import { NAME_MAX_LENGTH } from '../../TextFields/NameField';
+
+function CreateTeamViewStep(props) {
+  const { updateFormData, formData, marketId, setViewCreated } = props;
+  const intl = useIntl();
+  const value = formData.name || '';
+  const validForm = !_.isEmpty(value);
+  const classes = useContext(WizardStylesContext);
+  const [, setOperationRunning] = useContext(OperationInProgressContext);
+  const [, groupsDispatch] = useContext(MarketGroupsContext);
+  const [, diffDispatch] = useContext(DiffContext);
+  const [, groupMembersDispatch] = useContext(GroupMembersContext);
+
+  function onNameChange (event) {
+    const { value } = event.target;
+    updateFormData({
+      name: value
+    });
+  }
+
+  function createGroup() {
+    const dispatchers = {
+      groupsDispatch,
+      diffDispatch,
+      groupMembersDispatch
+    };
+
+    const groupData = {
+      ...formData,
+      marketId,
+    };
+    return doCreateGroup(dispatchers, groupData)
+      .then(() => {
+        setViewCreated(true);
+        setOperationRunning(false);
+      });
+  }
+
+  return (
+    <WizardStepContainer
+      {...props}
+    >
+      <Typography className={classes.introText}>
+        Do do you want a view for you and this collaborator?
+      </Typography>
+      <Typography className={classes.introSubText} variant="subtitle1" style={{paddingBottom: '1rem'}}>
+        Anything created in that view will notify this person. Otherwise you will have to @ mention them each time
+        you want them involved.
+      </Typography>
+      <OutlinedInput
+        id="groupName"
+        className={classes.input}
+        value={value}
+        autoFocus
+        onChange={onNameChange}
+        placeholder={intl.formatMessage({ id: 'GroupWizardMeetingName' })}
+        variant="outlined"
+        inputProps={{ maxLength : NAME_MAX_LENGTH }}
+        endAdornment={
+          <InputAdornment position={'end'} style={{ marginRight: '1rem' }}>
+            {NAME_MAX_LENGTH - (formData?.name?.length ?? 0)}
+          </InputAdornment>
+        }
+      />
+      <div className={classes.borderBottom} />
+      <WizardStepButtons
+        {...props}
+        validForm={validForm}
+        onNext={createGroup}
+        showSkip
+        />
+    </WizardStepContainer>
+  );
+}
+
+CreateTeamViewStep.propTypes = {
+  updateFormData: PropTypes.func,
+  formData: PropTypes.object,
+};
+
+CreateTeamViewStep.defaultProps = {
+  updateFormData: () => {},
+  formData: {},
+};
+
+export default CreateTeamViewStep;

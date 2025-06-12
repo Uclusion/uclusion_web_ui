6fd70f29f585863738ff9cb6fd4598ea011f39d6
disrael <disrael@gmail.com>
2025-06-12 07:41:27 -0700

    fix: guard on weird group state structure

--- src/contexts/MarketGroupsContext/marketGroupsContextHelper.js
+++ src/contexts/MarketGroupsContext/marketGroupsContextHelper.js
@@ -6,9 +6,14 @@ export function getGroup(state, marketId, groupId) {
   if (!marketId) {
     let group = undefined;
     Object.keys(groupsSafe).forEach((marketId) => {
-      const foundGroup = groupsSafe[marketId]?.find((group) => group.id === groupId);
-      if (foundGroup) {
-        group = foundGroup;
+      if (groupsSafe[marketId].find) {
+        const foundGroup = groupsSafe[marketId].find((group) => group.id === groupId);
+        if (foundGroup) {
+          group = foundGroup;
+        }
+      } else {
+        console.error(`Incorrect structure for group state and ${marketId}`);
+        console.error(groupsSafe[marketId]);
       }
     });
     return group;

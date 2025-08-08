e2259383f15e1d44eb0d0a9e3067670ebab7ec6e
disrael <disrael@gmail.com>
2025-08-08 16:28:56 -0700

    feat: update MyLink class to allow internal links without target attribute based on config settings

--- src/components/TextEditors/Utilities/LinkUtils.js
+++ src/components/TextEditors/Utilities/LinkUtils.js
@@ -1,11 +1,12 @@
 import Quill from 'quill';
+import config from '../../../config';
 
 const Link = Quill.import('formats/link');
 
 export class MyLink extends Link {
   static create(value) {
     const node = super.create(value);
-    if (!value?.startsWith('http')) {
+    if (!value?.startsWith('http')||value?.startsWith(config.ui_base_url)) {
       // See https://github.com/quilljs/quill/issues/1139 on removing target for internal links
       node.removeAttribute('target');
     }

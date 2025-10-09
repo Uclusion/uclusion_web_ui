fffec2e0f90d3742b21becf71876dff8353863ff
David Israel <disrael@uclusion.com>
2025-10-09 15:21:45 -0700

    refactor: refine empty state message in InboxExpansionPanel for enhanced clarity

--- src/pages/Home/YourWork/InboxExpansionPanel.js
+++ src/pages/Home/YourWork/InboxExpansionPanel.js
@@ -189,7 +189,7 @@ export function createDefaultInboxRow(messagesOrdered, tabIndex) {
   return (
     <Typography style={{marginTop: '2rem', maxWidth: '40rem', marginLeft: 'auto', marginRight: 'auto'}}
                 variant="body1">
-      Your For You tab is empty.<br/><br/> Notifications from others display here.
+      Your For You is empty.<br/><br/> Notifications from others display here.
     </Typography>
   );
 }

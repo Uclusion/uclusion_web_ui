86c0b10a2154068d0fc6fc8bd0069e60010b1f65
David Israel <disrael@uclusion.com>
2025-10-09 15:21:17 -0700

    refactor: update empty state message in InboxExpansionPanel for improved clarity

--- src/pages/Home/YourWork/InboxExpansionPanel.js
+++ src/pages/Home/YourWork/InboxExpansionPanel.js
@@ -181,7 +181,7 @@ export function createDefaultInboxRow(messagesOrdered, tabIndex) {
     return (
       <Typography style={{marginTop: '2rem', maxWidth: '40rem', marginLeft: 'auto', marginRight: 'auto'}}
                   variant="body1">
-        Your From You tab is empty.<br/><br/> Process or poke anything waiting on others here.
+        Your From You is empty.<br/><br/> Process or poke anything waiting on others here.
       </Typography>
     );
   }

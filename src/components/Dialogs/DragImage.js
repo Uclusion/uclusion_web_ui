e909c97bd311440dd5209fa98fcd688ae3c827f0
David Israel <disrael@uclusion.com>
2025-12-18 11:26:04 -0500

    fix: adjust DragImage component positioning for improved layout consistency

--- src/components/Dialogs/DragImage.js
+++ src/components/Dialogs/DragImage.js
@@ -6,7 +6,7 @@ export default function DragImage(properties) {
   const useName = name?.substring(0, 100);
   return (
     <div id={`dragImage${id}`} key={`dragImage${id}`} style={{display: 'block', minWidth: '10rem', width: '10rem',
-      position: 'absolute', top: -50, right: -10, zIndex: 2}}>
+      position: 'absolute', top: -70, right: -10, zIndex: 2}}>
       <Typography color='initial' variant="caption">{useName}</Typography>
     </div>
   );

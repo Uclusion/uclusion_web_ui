c78d43c7c8fd5b82b2784acd8d6fa02d0da0f2e9
David Israel <disrael@uclusion.com>
2025-12-31 07:57:35 -0800

    refactor: add LIGHTER_BLUE_COLOR constant and update usage in GmailTabItem and GmailTabs components for improved styling

--- src/components/Buttons/ButtonConstants.js
+++ src/components/Buttons/ButtonConstants.js
@@ -4,3 +4,4 @@ export const WARNING_COLOR = '#E85757';
 export const INFO_COLOR = '#DFF0F2';
 export const COUNT_COLOR = '#F29100';
 export const LIGHT_BLUE_COLOR = '#EDF7F8';
+export const LIGHTER_BLUE_COLOR = '#F8FBFC';

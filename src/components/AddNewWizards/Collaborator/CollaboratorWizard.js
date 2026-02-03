2b9d3e211e071eafc6739a5e167841b2f751260a
David Israel <disrael@uclusion.com>
2026-02-02 16:26:43 -0800

    refactor: remove unused PropTypes import in CollaboratorWizard for cleaner code

--- src/components/AddNewWizards/Collaborator/CollaboratorWizard.js
+++ src/components/AddNewWizards/Collaborator/CollaboratorWizard.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react';
-import PropTypes from 'prop-types'
 import { WizardStylesProvider } from '../WizardStylesContext'
 import FormdataWizard from 'react-formdata-wizard'
 import { extractUsersList } from '../../../utils/userFunctions';

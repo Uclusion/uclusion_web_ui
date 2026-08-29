55eff06e35d8a7fb752fb88d021eaaee62dbfc76
David Israel <disrael@uclusion.com>
2026-08-29 12:59:21 -0700

    hash was wrong

--- public/scripts/uclusionInstall.py
+++ public/scripts/uclusionInstall.py
@@ -145,7 +145,7 @@ SETUP_BOOTSTRAP_SCRIPT_SHA256 = {
     'uclusionSetupMCP.py':
         'f91ea798847ec8f8cb3407dfcc8eb4ab36ffbaab0c9695fb6028b56b94549d51',
     'uclusionCodexBridge.py':
-        'a532788c8aa2845c41b3a2c041144766667cb3196a210b25fe7d859cb885455d',
+        'f0900436fffaa7a46421cfef15e70e3d6cccd5149d0260d053e66f1955f115b2',
     'uclusionTokenAudit.py':
         '371e49d36c8393048f8e500bace829c9031f59f504673bdc40b1c1af12453df8',
     'uclusionCursorPokeDrain.py':
